733abf3e960c08edda0d2006bb6910198eca7d5a
Ahmed-pc <perfect_ahmed@yahoo.com>
2020-02-18 23:30:09 +0200

    change home / dashbaord

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -26,7 +26,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = RouteServiceProvider::RESUME_BUILDER;
 
     /**
      * Create a new controller instance.
