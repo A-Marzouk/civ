f7d5fc3c9e0f824f91197d4c5c0fdc76382bb1c1
Ahmed-pc <perfect_ahmed@yahoo.com>
2020-02-24 10:34:27 +0200

    register standalone page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,110 @@
-@extends('layouts.app')
+@extends('layouts.my-app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+    <div class="mt-5 mb-5">
+        <div class="form-wrap justify-content-center" style="background-image: url(/images/welcome_landing_page/icons/new-background.png);">
+            <div class="hold-form">
+            <span class="title-form">
+              Create new account
+            </span>
+                <div class="social-wrap">
+                    <a href="/register/instagram"><img src="/images/welcome_landing_page/icons/instagram.png" alt=""></a>
+                    <a href="/register/linkedin"><img src="/images/welcome_landing_page/icons/linkedin.png" alt=""></a>
+                    <a href="/register/google"><img src="/images/welcome_landing_page/icons/google.png" alt=""></a>
+                    <a href="/register/facebook"><img src="/images/welcome_landing_page/icons/facebook.png" alt=""></a>
+                    <a href="/register/github"><img src="/images/welcome_landing_page/icons/github.png" alt=""></a>
                 </div>
+                <span class="title-inline">
+                    or Sign up with Email
+                </span>
+                <form class="v-form"  method="POST" action="{{ route('register') }}">
+                    @csrf
+
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-sm-12 col-lg-12 col-12">
+                                <div class="v-input v-input--has-state theme--light v-text-field v-text-field--is-booted v-text-field--placeholder error--text">
+                                    <div class="v-input__control">
+                                        <div class="v-input__slot">
+                                            <div class="v-text-field__slot d-flex flex-column">
+                                                <input id="name" type="text"
+                                                       class="w-100 @error('name') is-invalid @enderror" name="name"
+                                                       value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Name">
+
+                                                @error('name')
+                                                <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-lg-12 col-12">
+                                <div class="v-input v-input--has-state theme--light v-text-field v-text-field--is-booted v-text-field--placeholder error--text">
+                                    <div class="v-input__control">
+                                        <div class="v-input__slot">
+                                            <div class="v-text-field__slot d-flex flex-column">
+                                                <input id="email" type="email"
+                                                       class="w-100 @error('email') is-invalid @enderror" name="email"
+                                                       value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="Email">
+
+                                                @error('email')
+                                                <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-lg-12 col-12">
+                                <div class="v-input v-input--has-state theme--light v-text-field v-text-field--is-booted v-text-field--placeholder error--text">
+                                    <div class="v-input__control">
+                                        <div class="v-input__slot">
+                                            <div class="v-text-field__slot d-flex flex-column">
+                                                <input id="password" type="password"
+                                                       class="w-100 @error('password') is-invalid @enderror" name="password" placeholder="Password"
+                                                       required autocomplete="current-password">
+
+                                                @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                                @enderror
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-lg-12 col-12">
+                                <div class="v-input v-input--has-state theme--light v-text-field v-text-field--is-booted v-text-field--placeholder error--text">
+                                    <div class="v-input__control">
+                                        <div class="v-input__slot">
+                                            <div class="v-text-field__slot d-flex flex-column">
+                                                <input id="password-confirm"  type="password"
+                                                       class="w-100 @error('password') is-invalid @enderror" name="password_confirmation"
+                                                       required autocomplete="new-password" placeholder="Confirm password">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+                            <div class="col-sm-12 col-lg-12 col-12 NoDecor">
+                                <button type="submit" class="btn-inset light__blue full"><i>Sign Up</i></button>
+                            </div>
+
+                        </div>
+                    </div>
+                </form>
+                <span class="title-inline">Already have account? <a href="/login">Login</a></span>
             </div>
         </div>
     </div>
-</div>
 @endsection
