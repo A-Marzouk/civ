bbdae29ac7af7aafd178b9e263823847e3601f05
Ahmed-pc <perfect_ahmed@yahoo.com>
2020-02-27 22:07:11 +0200

    api register user - using client_secret and client_id

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -10,14 +10,22 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\Hash;
 use App\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
+use Laravel\Passport\Client;
 
 class AuthController extends Controller
 {
     public $successStatus = 200;
+
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('register'); // user already received the access_token - he can login / logout
+    }
+
     /**
      * login api
      *
@@ -25,17 +33,9 @@ class AuthController extends Controller
      */
     public function login(){
 
-        $attempt = Auth::attempt(['email' => request('email'), 'password' => request('password')]);
-
-        if($attempt){
-            $user = Auth::user();
-
-            $success['access_token'] = $user->createToken($user->name. '| USER ID:' . $user->id)->accessToken;
-            $success['user'] =  $user;
-
-            return response()->json(['success' => $success], $this->successStatus);
+        if(Auth::check()){
+            return response()->json(['user' => Auth::user()], $this->successStatus);
         }
-
         return response()->json(['error'=>'Unauthorised'], 401);
     }
     /**
@@ -45,27 +45,35 @@ class AuthController extends Controller
      */
     public function register(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'string', 'min:8', 'confirmed'],
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json(['error'=>$validator->errors()], 401);
+        // check client_id and client_secret as first step:
+        if($request->client_id){
+            $client = Client::where('id',$request->client_id)->first();
+        }else{
+            return response()->json(['error'=> 'Wrong client credentials - No client ID provided.'], 401);
         }
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ])->assignRole('agent');
-
-        $success['access_token'] = $user->createToken($user->name. '| USER ID:' . $user->id)->accessToken;
-        $success['user'] =  $user;
-
-        return response()->json(['success'=>$success], $this-> successStatus);
+        if($request->client_secret === $client->secret && !$client->revoked){
+            $validator = Validator::make($request->all(), [
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => ['required', 'string', 'min:8', 'confirmed'],
+            ]);
+            if ($validator->fails()) {
+                return response()->json(['error'=>$validator->errors()], 401);
+            }
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ])->assignRole('agent');
+            $success['access_token'] = $user->createToken($user->name. '| USER ID:' . $user->id)->accessToken;
+            $success['user'] =  $user;
+            return response()->json(['success'=>$success], $this-> successStatus);
+        }else{
+            return response()->json(['error'=> 'Wrong client credentials.'], 401);
+        }
     }
+
     /**
      * details api
      *
