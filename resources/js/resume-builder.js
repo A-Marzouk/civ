82d70e4fd414ada6c7864bc353ce3f393f1e3c4b
José Daniel  Quintero <josedanq100@gmail.com>
2020-03-05 14:49:36 -0400

    fixed vuejs version conflicts

--- resources/js/resume-builder.js
+++ resources/js/resume-builder.js
@@ -65,22 +65,22 @@ if ($("#resumeBuilder").length !== 0) {
                     children: [
                         {
                             path:'',
-                            component: require('./components/resume_builder/tabs/edit_tabs/projects_views/projects.vue'),
+                            component: require('./components/resume_builder/tabs/edit_tabs/projects_views/projects.vue').default,
                             props: true
                         },
                         {
                             path:'step-1',
-                            component: require('./components/resume_builder/tabs/edit_tabs/projects_views/form_view_num1.vue'),
+                            component: require('./components/resume_builder/tabs/edit_tabs/projects_views/form_view_num1.vue').default,
                             props: true
                         },
                         {
                             path:'step-2',
-                            component: require('./components/resume_builder/tabs/edit_tabs/projects_views/form_view_num2.vue'),
+                            component: require('./components/resume_builder/tabs/edit_tabs/projects_views/form_view_num2.vue').default,
                             props: true
                         },
                         {
                             path:'step-3',
-                            component: require('./components/resume_builder/tabs/edit_tabs/projects_views/form_view_num3.vue'),
+                            component: require('./components/resume_builder/tabs/edit_tabs/projects_views/form_view_num3.vue').default,
                             props: true
                         }
                     ]
