37073ed698e594d33db32393170713aa7df9b8ac
Ahmed-pc <perfect_ahmed@yahoo.com>
2020-03-07 23:13:12 +0200

    Projects controller - CRUD

--- app/Http/Controllers/API/ProjectsController.php
+++ app/Http/Controllers/API/ProjectsController.php
@@ -7,6 +7,7 @@ use App\Project;
 use Illuminate\Http\Request;
 use App\Http\Resources\Project as ProjectResource;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 
 class ProjectsController extends Controller
@@ -27,69 +28,54 @@ class ProjectsController extends Controller
         $projects = Project::where('user_id',Auth::user()->id)->paginate(5);
         return ProjectResource::collection($projects);
     }
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+
+        $this->validator($request->all())->validate();
+
+        if($request->isMethod('put')){
+            // update
+            $project = Project::findOrFail($request->id);
+            $project->update($request->toArray());
+        }else{
+            // add
+            $request['user_id'] = Auth::user()->id;
+            $project = Project::create($request->toArray());
+        }
+
+        if ($project->id){
+            return new ProjectResource($project);
+        }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        //
+        $project = Project::where([
+            'id' => $id,
+            'user_id' => Auth::user()->id
+        ])->first();
+
+        return new ProjectResource($project);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        $project = Project::where([
+            'id' => $id,
+            'user_id' => Auth::user()->id
+        ])->first();
+
+        if($project->delete()){
+            return ['data' => ['id' => $project->id] ];
+        }
+    }
+
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string', 'max:255','min:3'],
+            'description' => ['required', 'string', 'max:2500'],
+            'link' => ['required', 'string','max:255'],
+        ]);
     }
 }
