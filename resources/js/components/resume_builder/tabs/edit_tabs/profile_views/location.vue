3efd1c2b78ae49f1c9a1d22c9b8195e64e06240e
Ahmed-pc <perfect_ahmed@yahoo.com>
2020-03-09 17:03:45 +0200

    Location tab front

--- resources/js/components/resume_builder/tabs/edit_tabs/profile_views/location.vue
+++ resources/js/components/resume_builder/tabs/edit_tabs/profile_views/location.vue
@@ -2,30 +2,28 @@
     <div>
         <div class="hold-tab wrapp">
             <div class="input-field">
-                <label for="profilelink">New location</label>
-                <input type="text" placeholder="" name="profilelink">
+                <label for="location">Location</label>
+                <input type="text" name="location" id="location">
             </div>
-            <a href="#" class="btn-blue"><img src="/images/resume_builder/profile/icon-check.png">Add location now</a>
-            <a href="" class="btn-outline ml-5">Auto import</a>
-            <ul class="lang-items">
-                <li class="lang-item">
-                    <img class="flagLang" src="/images/resume_builder/profile/flag-france.png" alt="">
-                    <span class="nameLang">French</span>
-                    <span class="input-select dropdown __md">
-                            <button class="audio-options dropdown-toggle" type="button" id="dropdownMenuButton4" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Option
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                <a class="dropdown-item" href="#">
-                                    <svg-vue class="option-icon" icon="edit-icon"></svg-vue>Edit
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    <svg-vue class="option-icon" icon="trash-delete-icon"></svg-vue>Delete
-                                </a>
-                            </div>
-                        </span>
-                </li>
-            </ul>
-            </div>
+            <a href="javascript:void(0)" class="btn-blue"><img alt="location" src="/images/resume_builder/profile/icon-check.png">Save location</a>
+        </div>
     </div>
 </template>
+
+<script>
+    export default {
+        data(){
+            return{
+
+            }
+        },
+        methods:{
+
+        },
+        computed: {
+            location() {
+                return this.$store.state.user;
+            }
+        },
+    }
+</script>
