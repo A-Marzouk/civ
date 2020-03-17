da52400f6152fa84c1de053a86cfa12e7898f341
Nishad Islam <nishad.bdg@gmail.com>
2020-03-17 10:35:48 +0600

    audio video tab added

---
+++ resources/js/components/resume_builder/tabs/edit_tabs/audio_views/Audio.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <div class="content d-flex">
+      <div class="upload-container d-flex flex-column">
+        <h3 class="text-blue">Upload my audio</h3>
+        <vue2Dropzone id="audio_and_video" :options="dropzoneOptions" :useCustomSlot="true">
+          <svg-vue class="upload-audio-icon" icon="upload-audio-icon"></svg-vue>
+          <div class="empty-text">
+            Drag and drop audio files
+            <br />or
+          </div>
+          <button class="btn filled btn-upload">
+            <svg-vue class="upload-icon icon" icon="upload-icon"></svg-vue>
+            <span>Browse audio file</span>
+          </button>
+        </vue2Dropzone>
+        <!-- Insert icon -->
+      </div>
+
+      <!-- Audio previews -->
+      <div class="audios-preview-container">
+        <div
+          v-for="(mediaElement,index) in media"
+          :key="index + '_mediaElement'"
+          v-if="mediaElement.type === 'audio'"
+        >
+          <audio-element :mediaElement="mediaElement"></audio-element>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dropzoneOptions: {
+        url: "https://httpbin.org/post",
+        thumbnailWidth: 150,
+        maxFilesize: 0.5,
+        acceptedFiles: "audio/*"
+      },
+      audios: [{}],
+      errors: {}
+    };
+  },
+  methods: {},
+  computed: {}
+};
+</script>
+
+<style scoped lang="scss">
+$mainBlue: #001ce2;
+
+.upload-container {
+  position: relative;
+  width: 354px;
+  margin-right: 232px;
+
+  h3 {
+    font-size: 28px;
+    position: relative;
+    width: 100%;
+    text-align: center;
+
+    &::after {
+      position: absolute;
+      bottom: -30px;
+      left: 0;
+      content: "";
+      width: 100%;
+      height: 2px;
+      background: $mainBlue;
+    }
+  }
+}
+
+.vue-dropzone {
+  box-sizing: border-box;
+  max-width: 100%;
+  padding-top: 30px;
+  height: 225px;
+  margin-top: 98px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.upload-audio-icon {
+  height: 48px;
+  margin-bottom: 10px;
+}
+
+.upload-icon {
+  width: 25px;
+}
+
+.btn-upload {
+  width: 214px;
+  margin: 0 auto;
+}
+
+.audios-preview-container {
+  margin-top: 10px;
+}
+</style>
--- resources/js/modules.js
+++ resources/js/modules.js
@@ -23,3 +23,9 @@ Vue.use(SvgVue);
 // cookies:
 import VueCookies from 'vue-cookies'
 Vue.use(VueCookies);
+
+//bootstrap vue
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
