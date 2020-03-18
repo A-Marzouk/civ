b4b79b09f9371f575baf2902bcf05b0df7cf9c62
Ahmed-pc <perfect_ahmed@yahoo.com>
2020-03-18 09:19:45 +0200

    Update acorn package for security reasons

--- resources/js/components/resume_builder/tabs/edit_tabs/achievements_views/public.vue
+++ resources/js/components/resume_builder/tabs/edit_tabs/achievements_views/public.vue
@@ -182,7 +182,7 @@ export default {
                     this.$store.dispatch('fullScreenNotification');
                     this.clearPublic();
                 })
-                .catch((error) =>
+                .catch((error) =>{
                     if (typeof error.response.data === 'object') {
                         this.errors.new = error.response.data.errors;
                     } else {
