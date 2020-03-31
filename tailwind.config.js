e27b312738a3ae5782579981e9873cf8524ce4d0
Ayoub JAMOUHI <gemouhi@gmail.com>
2020-03-31 16:43:43 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,36 @@
-const { colors } = require('tailwindcss/defaultTheme');
-
 module.exports = {
     theme: {
         extend: {
+            spacing: {
+                'tm39-1': '158px',
+                'tm39-2': '132px',
+                'tm39-3': '5px',
+                'tm39-4': '915px',
+                'tm39-5': '14px',
+                'tm39-6': '117px',
+                'tm39-7': '15px',
+            },
             colors: {
-                gray: {
-                    ...colors.gray,
-                    '301': '#EBEBEB',
-                    '302': '#D9D9D9',
-                    '701': '#444444',
-                    '702': '#484848',
+                tm39: {
+                    '1': '#513ECD',
+                    '2': '#EBEBEB',
+                    '3': '#D9D9D9',
+                    '4': '#444444',
+                    '5': '#484848',
+                    '6': '#2D2D2D',
                 },
-                purple: {
-                    ...colors.purple,
-                    '501': '#513ECD',
-                },
+            },
+            fontFamily: {
+                lato: ['Lato', 'sans-serif'],
+            },
+            maxWidth: {
+                'tm39-dw-1': '73.875rem',
+            },
+            gridTemplateColumns: {
+                'tm39-d-skills-bar': '118px auto 48px',
+            },
+            borderRadius: {
+                'tm39-15': '15px',
             },
         },
     },
