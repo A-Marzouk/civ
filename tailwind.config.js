7893dc3b39467bfc785cc0964222c837b270c1d0
Ayoub JAMOUHI <gemouhi@gmail.com>
2020-04-02 21:30:55 +0900

    Edit spacing config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,38 @@
+const spacing = {
+    'tm700-1': '158px',
+    'tm700-2': '132px',
+    'tm700-3': '5px',
+    'tm700-4': '915px',
+    'tm700-5': '14px',
+    'tm700-6': '117px',
+    'civ-15': '15px',
+    'tm700-8': '94px',
+    'tm700-9': '100px',
+    'tm700-10': '25px',
+    'tm700-11': '9px',
+    'civ-10': '10‬px',
+    'civ-13': '13px',
+    'civ-18': '18px',
+    'civ-21': '21‬px',
+    'civ-61': '61‬px',
+    '9px': '9px',
+    '15px': '15px',
+    '18px': '18px',
+    '20px': '20px',
+    '22px': '22px',
+    '47px': '1.68rem',
+    '60px': '3.75rem',
+    '66px': '4.13rem',
+    '52px': '4.13rem',
+    '52px': '4.13rem',
+    '80px': '5rem', // 80px
+    '88px': '5.5rem', // 80px
+    '96px': '6rem', // 80px
+};
 module.exports = {
     theme: {
         extend: {
-            spacing: {
-                'tm700-1': '158px',
-                'tm700-2': '132px',
-                'tm700-3': '5px',
-                'tm700-4': '915px',
-                'tm700-5': '14px',
-                'tm700-6': '117px',
-                'tm700-7': '15px',
-                'tm700-8': '94px',
-            },
+            spacing,
             colors: {
                 tm700: {
                     '1': '#513ECD',
@@ -24,7 +46,9 @@ module.exports = {
             },
             inset: {
                 '24px': '24px',
+                '34px': '34px',
                 '94px': '94px',
+                'minus-80px': '-80px',
             },
             fontFamily: {
                 lato: ['Lato', 'sans-serif'],
@@ -39,16 +63,28 @@ module.exports = {
                 'tm700-d-skills-bar': '118px auto 48px',
             },
             borderRadius: {
-                'tm700-15': '15px',
+                'civ-15': '15px',
+                'civ-50': '3.125‬‬rem', // 50px
+                'civ-50p': '50px', // 50px
             },
             flex: {
-                'tm700-1': '0.53',
-                'tm700-2': '1.2',
-                'tm700-3': '1.4',
-                'tm700-4': '1.7',
+                'civ-1': '0.53',
+                'civ-1_2': '1.2',
+                'civ-3': '1.4',
+                'civ-4': '1.7',
+                'civ-1_5': '1.5',
+                'civ-2': '2',
             },
             borderWidth: {
                 '14': '14px',
+            },
+            fontSize: {
+                'tm700-18': '1.125rem', // 18px
+                'tm700-22': '1.375‬rem', // 22px
+            },
+            lineHeight: {
+                'civ-22': '22px', // 22px
+                'civ-29': '29‬px', // 29px
             },
         },
     },
