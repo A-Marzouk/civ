266064659c8f8c2afbf00d5cad4edc89d031c745
Nishad Islam <nishad.bdg@gmail.com>
2020-04-04 20:45:02 +0600

    social buttons added

--- resources/js/components/resume_themes/theme206.vue
+++ resources/js/components/resume_themes/theme206.vue
@@ -6,8 +6,8 @@
           <v-img src="/images/resume_themes/theme206/avatar.png"></v-img>
         </v-avatar>
         <div class="half-circle"></div>
-        <v-row class="mx-3">
-          <v-col md="7">
+        <v-row class="mx-3" align="center" justify="center">
+          <v-col md="4">
             <v-card flat color="transparent">
               <v-card-title class="custom-profile-title">Hean Prinsloo</v-card-title>
               <v-card-subtitle class="custom-profile-subtitle">Graphic Designer</v-card-subtitle>
@@ -16,25 +16,66 @@
               >Donec a augue gravida, vulputate ligula et, pellentesque arcu. Morbi feugiat eros nec sem ultrices...</v-card-text>
             </v-card>
           </v-col>
-          <v-col md="5">
-            <v-card flat color="transparent">
-              <div class="d-flex flex-row">
-                <div class="mr-3 d-flex flex-column">
-                  <div class="p-2"><v-icon>fa-dollar-sign</v-icon></div>
-                  <div class="p-2">Flex item 1</div>
-                </div>
-                <div class="mr-3 d-flex flex-column">
-                  <div class="p-2">Flex item 1</div>
-                  <div class="p-2">Flex item 1</div>
-                </div>
-
-                <div class="mr-3 d-flex flex-column">
-                  <div class="p-2">Flex item 1</div>
-                  <div class="p-2">Flex item 1</div>
-                </div>
+          <v-col md="4">
+            <div class="d-flex flex-row">
+              <!-- Hour rate -->
+              <div class="">
+                <v-list-item two-line>
+                  <v-list-item-avatar size="18">
+                    <img width="18" src="/images/resume_themes/theme206/icons/usd.png" />
+                  </v-list-item-avatar>
+                  <v-list-item-content>
+                    <v-list-item-subtitle>
+                      <v-card color="transparent" flat>
+                        <span class="hour-rate">Hour Rate</span>
+                      </v-card>
+                    </v-list-item-subtitle>
+                    <v-list-item-subtitle>
+                      <v-card color="transparent" flat>
+                        <span class="rate">20</span>
+                      </v-card>
+                    </v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
               </div>
+              <!-- Hour Rate -->
+              <!-- Weekly availibility -->
+              <div class="">
+                <v-list-item two-line>
+                  <v-list-item-avatar size="18">
+                    <img width="18" src="/images/resume_themes/theme206/icons/usd.png" />
+                  </v-list-item-avatar>
+                  <v-list-item-content>
+                    <v-list-item-subtitle>
+                      <v-card color="transparent" flat>
+                        <span class="hour-rate">Weekly availibility</span>
+                      </v-card>
+                    </v-list-item-subtitle>
+                    <v-list-item-subtitle>
+                      <v-card color="transparent" flat>
+                        <span class="rate">20</span>
+                      </v-card>
+                    </v-list-item-subtitle>
+                  </v-list-item-content>
+                </v-list-item>
+              </div>
+              <!-- Weekly availibility -->
+            </div>
+          </v-col>
+
+          <!-- 3rd column -->
+          <v-col md="4">
+            <v-card flat color="transparent">
+              <v-btn color="#FAFAFA" x-large>
+                <v-icon color="#5843BE" small left>mdi-email</v-icon>Hire Me
+              </v-btn>
+
+              <!-- social buttons -->
+              <!-- social buttons -->
+
             </v-card>
           </v-col>
+          <!-- 3rd column -->
         </v-row>
       </v-app-bar>
     </v-container>
@@ -42,7 +83,22 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      socialIcons: [
+        { id: 1, title: "twitter" },
+        { id: 2, title: "facebook" },
+        { id: 3, title: "instagram" }
+      ]
+    };
+  },
+  methods: {
+    getSocialIcon(title) {
+      return `/images/resume_themes/theme206/social_icons/${title}.webp`;
+    }
+  }
+};
 </script>
 
 <style scoped lang="scss">
@@ -65,8 +121,21 @@ export default {};
 
 .custom-profile-text {
   font-family: "Poppins", sans-serif !important;
-  color: #000000;
-  font-size: 1.125rem;
+  color: #000000 !important;
+  font-size: 0.9rem;
   line-height: 1.6888rem;
+}
+
+.hour-rate {
+  font-family: "Poppins", sans-serif !important;
+  color: rgba(88, 67, 190, 0.5) !important;
+  font-size: 0.75rem;
+  line-height: 1.313rem;
+}
+.rate {
+  font-family: "Poppins", sans-serif !important;
+  color: #5843be !important;
+  font-size: 2.5rem;
+  line-height: 3.75rem;
 }
 </style>
