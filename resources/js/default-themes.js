3cd9b76df067835d1dfb68e823ee6f0323316454
Lewiz Acosta <mp3edw@gmail.com>
2020-04-14 23:22:11 -0500

    Theme 307 added (Desktop version) Working responsive

--- resources/js/default-themes.js
+++ resources/js/default-themes.js
@@ -309,3 +309,15 @@ if ($("#resumeTheme206").length !== 0) {
         }
     });
 }
+import resumeTheme307 from './components/resume_themes/theme307';
+if ($("#resumeTheme307").length !== 0) {
+
+    new Vue({
+        el: '#resumeTheme307',
+        vuetify,
+        components: {
+            'resume-theme-307': resumeTheme307
+        }
+    });
+
+}
