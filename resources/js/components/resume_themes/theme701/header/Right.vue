fd19b18c262c335300e07260e13c00975c96aa0e
Ayoub JAMOUHI <gemouhi@gmail.com>
2020-04-17 19:29:21 +0900

    Fix size icons

--- resources/js/components/resume_themes/theme701/header/Right.vue
+++ resources/js/components/resume_themes/theme701/header/Right.vue
@@ -1,23 +1,70 @@
 <template>
-  <div class="tm701-right ">
+  <div class="tm701-right">
     <div class="chat xl:tw-pb-46px">
       <div class="xl:tw-flex xm:tw-flex-row xl:tw-justify-end">
-        <button class="tw-bg-white xl:tw-py-22px xl:tw-pl-70px xl:tw-pr-36px xl:tw-rounded-lg xl:tw-mr-26px">
-          <svg class="xl:tw-inline xl:tw-pr-9px" id="Group_3" data-name="Group 3" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-            <rect id="bound" width="24" height="24" fill="none"/>
-            <path id="check" d="M2.875,5.75A.946.946,0,0,1,2.2,5.463L.287,3.546a.926.926,0,0,1,0-1.342.926.926,0,0,1,1.342,0L2.875,3.45,6.037.287a.926.926,0,0,1,1.342,0,.926.926,0,0,1,0,1.342L3.546,5.463A.946.946,0,0,1,2.875,5.75Z" transform="translate(2 15)" fill="#28404a"/>
-            <path id="Combined_Shape" data-name="Combined Shape" d="M16,13H8.984A7.507,7.507,0,0,0,0,6.15V2A2,2,0,0,1,2,0H16a2,2,0,0,1,2,2v9A2,2,0,0,1,16,13ZM2.5,1.75a.75.75,0,0,0-.356,1.411l6.5,3.5a.747.747,0,0,0,.711,0l6.5-3.5a.75.75,0,1,0-.711-1.321L9,5.148,2.856,1.84A.752.752,0,0,0,2.5,1.75Z" transform="translate(4 5)" fill="#28404a"/>
+        <button
+          class="tw-bg-white xl:tw-py-22px xl:tw-pl-70px xl:tw-pr-36px xl:tw-rounded-lg xl:tw-mr-26px"
+        >
+          <svg
+            class="xl:tw-inline"
+            id="Group_3"
+            data-name="Group 3"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <rect id="bound" width="24" height="24" fill="none" />
+            <path
+              id="check"
+              d="M2.875,5.75A.946.946,0,0,1,2.2,5.463L.287,3.546a.926.926,0,0,1,0-1.342.926.926,0,0,1,1.342,0L2.875,3.45,6.037.287a.926.926,0,0,1,1.342,0,.926.926,0,0,1,0,1.342L3.546,5.463A.946.946,0,0,1,2.875,5.75Z"
+              transform="translate(2 15)"
+              fill="#28404a"
+            />
+            <path
+              id="Combined_Shape"
+              data-name="Combined Shape"
+              d="M16,13H8.984A7.507,7.507,0,0,0,0,6.15V2A2,2,0,0,1,2,0H16a2,2,0,0,1,2,2v9A2,2,0,0,1,16,13ZM2.5,1.75a.75.75,0,0,0-.356,1.411l6.5,3.5a.747.747,0,0,0,.711,0l6.5-3.5a.75.75,0,1,0-.711-1.321L9,5.148,2.856,1.84A.752.752,0,0,0,2.5,1.75Z"
+              transform="translate(4 5)"
+              fill="#28404a"
+            />
           </svg>
           <span class="tw-text-18px tw-leading-23px">Start A Chat!</span>
         </button>
         <button class="tw-bg-white xl:tw-py-22px xl:tw-pl-52px xl:tw-pr-18px xl:tw-rounded-lg">
-          <svg class="xl:tw-inline xl:tw-pr-9px" id="Group_3" data-name="Group 3" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-            <rect id="bound" width="24" height="24" fill="none"/>
+          <svg
+            class="xl:tw-inline"
+            id="Group_3"
+            data-name="Group 3"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+          >
+            <rect id="bound" width="24" height="24" fill="none" />
             <g id="suitcase-with-white-details" transform="translate(4 -33.824)">
               <g id="Group_10323" data-name="Group 10323" transform="translate(0 36.548)">
-                <path id="Path_4840" data-name="Path 4840" d="M.681,110.315A2.225,2.225,0,0,0,0,111.946v8.585a2.224,2.224,0,0,0,.681,1.63,2.224,2.224,0,0,0,1.63.681h.66V109.634h-.66A2.224,2.224,0,0,0,.681,110.315Z" transform="translate(0 -106.992)" fill="#28404a"/>
-                <path id="Path_4841" data-name="Path 4841" d="M118.882,37.539a.986.986,0,0,0-.99-.991h-5.944a.986.986,0,0,0-.991.991V39.19h-1.321V52.4H120.2V39.19h-1.321Zm-1.321,1.651h-5.283V37.869h5.283Z" transform="translate(-105.673 -36.548)" fill="#28404a"/>
-                <path id="Path_4842" data-name="Path 4842" d="M431.691,110.315a2.225,2.225,0,0,0-1.63-.681h-.66v13.208h.66a2.325,2.325,0,0,0,2.311-2.311v-8.585A2.225,2.225,0,0,0,431.691,110.315Z" transform="translate(-413.88 -106.992)" fill="#28404a"/>
+                <path
+                  id="Path_4840"
+                  data-name="Path 4840"
+                  d="M.681,110.315A2.225,2.225,0,0,0,0,111.946v8.585a2.224,2.224,0,0,0,.681,1.63,2.224,2.224,0,0,0,1.63.681h.66V109.634h-.66A2.224,2.224,0,0,0,.681,110.315Z"
+                  transform="translate(0 -106.992)"
+                  fill="#28404a"
+                />
+                <path
+                  id="Path_4841"
+                  data-name="Path 4841"
+                  d="M118.882,37.539a.986.986,0,0,0-.99-.991h-5.944a.986.986,0,0,0-.991.991V39.19h-1.321V52.4H120.2V39.19h-1.321Zm-1.321,1.651h-5.283V37.869h5.283Z"
+                  transform="translate(-105.673 -36.548)"
+                  fill="#28404a"
+                />
+                <path
+                  id="Path_4842"
+                  data-name="Path 4842"
+                  d="M431.691,110.315a2.225,2.225,0,0,0-1.63-.681h-.66v13.208h.66a2.325,2.325,0,0,0,2.311-2.311v-8.585A2.225,2.225,0,0,0,431.691,110.315Z"
+                  transform="translate(-413.88 -106.992)"
+                  fill="#28404a"
+                />
               </g>
             </g>
           </svg>
@@ -27,31 +74,43 @@
     </div>
     <div class="hire">
       <div class="xl:tw-flex xl:tw-flex-row xl:tw-justify-end xl:items-center">
-          <div class="hourly-rate xl:tw-flex xl:tw-flex-col xl:tw-mr-8">
-            <span class="tw-text-26px tw-font-bold tw-leading-33px xl:tw-mb-2">$45</span>
-            <span class="tw-text-15px tw-leading-19px">Hourly Rate</span>
-          </div>
-          <div class="weekly-avaibility xl:tw-flex xl:tw-flex-col xl:tw-mr-8">
-            <span class="tw-text-26px tw-font-bold tw-leading-33px xl:tw-mb-2">45 Hr</span>
-            <span class="tw-text-15px tw-leading-19px">Weekly Avaibility</span>
-          </div>
-          <div>
-            <button class="tw-bg-tm701-4 xl:tw-py-22px xl:tw-pl-84px xl:tw-pr-70px xl:tw--lg-lg xl:tw-rounded-lg">
-              <svg class="xl:tw-inline xl:tw-mr-10px" xmlns="http://www.w3.org/2000/svg" width="19" height="21" viewBox="0 0 19 21">
-                <path id="Combined_Shape" data-name="Combined Shape" d="M16,21H3a3,3,0,0,1-3-3V3A3,3,0,0,1,3,0H16a3,3,0,0,1,3,3V18A3,3,0,0,1,16,21ZM9.49,10.5a4.873,4.873,0,0,0-5.267,4.724.65.65,0,0,0,.2.377.4.4,0,0,0,.229.149h9.688a.425.425,0,0,0,.323-.119.559.559,0,0,0,.117-.406C14.621,12.31,12.595,10.5,9.49,10.5ZM9.5,5.25a2.1,2.1,0,1,0,0,4.2c1.153,0,2.1-2.079,2.111-2.1C11.6,7.33,10.65,5.25,9.5,5.25Z" transform="translate(0 0)" fill="#fff"/>
-              </svg>
-              <span class="tw-text-white">Hire Me</span>
-            </button>
-          </div>
+        <div class="hourly-rate xl:tw-flex xl:tw-flex-col xl:tw-mr-8">
+          <span class="tw-text-26px tw-font-bold tw-leading-33px xl:tw-mb-2">$45</span>
+          <span class="tw-text-15px tw-leading-19px">Hourly Rate</span>
         </div>
-      </div>  
+        <div class="weekly-avaibility xl:tw-flex xl:tw-flex-col xl:tw-mr-8">
+          <span class="tw-text-26px tw-font-bold tw-leading-33px xl:tw-mb-2">45 Hr</span>
+          <span class="tw-text-15px tw-leading-19px">Weekly Avaibility</span>
+        </div>
+        <div>
+          <button
+            class="tw-bg-tm701-4 xl:tw-py-22px xl:tw-pl-84px xl:tw-pr-70px xl:tw--lg-lg xl:tw-rounded-lg"
+          >
+            <svg
+              class="xl:tw-inline xl:tw-mr-10px"
+              xmlns="http://www.w3.org/2000/svg"
+              width="19"
+              height="21"
+              viewBox="0 0 19 21"
+            >
+              <path
+                id="Combined_Shape"
+                data-name="Combined Shape"
+                d="M16,21H3a3,3,0,0,1-3-3V3A3,3,0,0,1,3,0H16a3,3,0,0,1,3,3V18A3,3,0,0,1,16,21ZM9.49,10.5a4.873,4.873,0,0,0-5.267,4.724.65.65,0,0,0,.2.377.4.4,0,0,0,.229.149h9.688a.425.425,0,0,0,.323-.119.559.559,0,0,0,.117-.406C14.621,12.31,12.595,10.5,9.49,10.5ZM9.5,5.25a2.1,2.1,0,1,0,0,4.2c1.153,0,2.1-2.079,2.111-2.1C11.6,7.33,10.65,5.25,9.5,5.25Z"
+                transform="translate(0 0)"
+                fill="#fff"
+              />
+            </svg>
+            <span class="tw-text-white">Hire Me</span>
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="scss">
