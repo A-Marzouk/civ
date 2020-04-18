c0c6438569309824f4ed1b4e381ddf60cf94fc46
Ayoub JAMOUHI <gemouhi@gmail.com>
2020-04-18 20:01:34 +0900

    Linting Left.vue

--- resources/js/components/resume_themes/theme701/header/Left.vue
+++ resources/js/components/resume_themes/theme701/header/Left.vue
@@ -2,7 +2,7 @@
   <div class="tm701-left">
     <img class="xl:tw-rounded-full xl:tw-max-w-xs" v-bind:src="logo" />
   </div>
-  </template>
+</template>
 
 <script>
 export default {
@@ -10,6 +10,6 @@ export default {
     return {
       logo: "/images/resume_themes/theme701/profile.png"
     };
-  },
-}
+  }
+};
 </script>
