7742ba5efc45fb67f3aaa7602d4178580964dbeb
Abdessamad El Hamdany <abdessamadelhamdany@gmail.com>
2020-04-23 11:25:17 +0000

    Fix Sidebar scrolling issue in mobile view

--- resources/js/components/resume_themes/theme1000/Sidebar.vue
+++ resources/js/components/resume_themes/theme1000/Sidebar.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="sidebar-wrapper tw-overflow-hidden tw-z-30" :class="{'open': isOpen}" ref="sidebar" @click="onClose">
-		<div class="sidebar tw-relative">
+		<div class="sidebar tw-relative tw-overflow-x-hidden tw-pb-10">
 			<div class="tw-absolute tw-top-0 tw-right-0 tw-text-white tw-p-5px tw-mt-20px tw-mr-25px tw-cursor-pointer" @click="$emit('onClose')">
 				<svg viewBox="0 0 9 10" class="tw-w-10px tw-h-10px tw-fill-current" xmlns="http://www.w3.org/2000/svg">
 					<path d="M9.19998 0.806647C8.93998 0.546647 8.51998 0.546647 8.25998 0.806647L4.99998 4.05998L1.73998 0.79998C1.47998 0.53998 1.05998 0.53998 0.79998 0.79998C0.53998 1.05998 0.53998 1.47998 0.79998 1.73998L4.05998 4.99998L0.79998 8.25998C0.53998 8.51998 0.53998 8.93998 0.79998 9.19998C1.05998 9.45998 1.47998 9.45998 1.73998 9.19998L4.99998 5.93998L8.25998 9.19998C8.51998 9.45998 8.93998 9.45998 9.19998 9.19998C9.45998 8.93998 9.45998 8.51998 9.19998 8.25998L5.93998 4.99998L9.19998 1.73998C9.45331 1.48665 9.45331 1.05998 9.19998 0.806647Z" />
