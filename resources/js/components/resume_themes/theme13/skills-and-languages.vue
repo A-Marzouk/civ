12a75ec241cbb91e0fe359cfd5732811a929df6d
José Daniel  Quintero <josedanq100@gmail.com>
2020-04-27 17:25:28 -0400

    theme13 icons

--- resources/js/components/resume_themes/theme13/skills-and-languages.vue
+++ resources/js/components/resume_themes/theme13/skills-and-languages.vue
@@ -7,33 +7,33 @@
             </div>
         </div>
         <div class="skill">
-            <img src="/images/resume_themes/theme13/ux-icon.png" alt="">
+            <img src="/images/resume_themes/theme13/usability-icon.png" alt="">
             <div class="skill-info">
-                <div class="skill-name">User Experience</div> <div class="percentage">85%</div>
+                <div class="skill-name">Usability</div> <div class="percentage">80%</div>
+            </div>
+        </div>
+        <div class="skill">
+            <img src="/images/resume_themes/theme13/interaction-icon.png" alt="">
+            <div class="skill-info">
+                <div class="skill-name">Interaction</div> <div class="percentage">75%</div>
+            </div>
+        </div>
+        <div class="skill">
+            <img src="/images/resume_themes/theme13/graphic-design-icon.png" alt="">
+            <div class="skill-info">
+                <div class="skill-name">Graphic Design</div> <div class="percentage">85%</div>
+            </div>
+        </div>
+        <div class="skill">
+            <img src="/images/resume_themes/theme13/development-icon.png" alt="">
+            <div class="skill-info">
+                <div class="skill-name">Development</div> <div class="percentage">50%</div>
             </div>
         </div>
         <div class="skill">
             <img src="/images/resume_themes/theme13/ux-icon.png" alt="">
             <div class="skill-info">
-                <div class="skill-name">User Experience</div> <div class="percentage">85%</div>
-            </div>
-        </div>
-        <div class="skill">
-            <img src="/images/resume_themes/theme13/ux-icon.png" alt="">
-            <div class="skill-info">
-                <div class="skill-name">User Experience</div> <div class="percentage">85%</div>
-            </div>
-        </div>
-        <div class="skill">
-            <img src="/images/resume_themes/theme13/ux-icon.png" alt="">
-            <div class="skill-info">
-                <div class="skill-name">User Experience</div> <div class="percentage">85%</div>
-            </div>
-        </div>
-        <div class="skill">
-            <img src="/images/resume_themes/theme13/ux-icon.png" alt="">
-            <div class="skill-info">
-                <div class="skill-name">User Experience</div> <div class="percentage">85%</div>
+                <div class="skill-name">Research</div> <div class="percentage">35%</div>
             </div>
         </div>
     </div>
