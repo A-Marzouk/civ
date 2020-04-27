5c7656386c0115c42b55cef46d03f4319d1e8c0a
Abdessamad El Hamdany <abdessamadelhamdany@gmail.com>
2020-04-27 14:38:07 +0000

    theme 1000 Adjusted Media Tab for desktop view

--- resources/js/components/resume_themes/theme1000/tabs/Media.vue
+++ resources/js/components/resume_themes/theme1000/tabs/Media.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="tw-px-4 tw-pt-16 tw-pb-12 md:tw-px-37px md:tw-grid md:tw-grid-cols-2 md:tw-gap-5">
+	<div class="tw-px-4 tw-pt-16 tw-pb-12 md:tw-px-37px md:tw-grid md:tw-grid-cols-2 md:tw-gap-5 xxl:tw-grid-cols-4">
 		<div class="tw-mb-5 md:tw-mb-0" v-for="(item, index) in media" :key="item.id" :style="getItemStyle(index)">
 			<AudioPreview v-if="item.type == 'audio'" :media="item" />
 			<VideoPreview v-else-if="item.type == 'video'" :media="item" />
@@ -124,9 +124,62 @@ export default {
 					artist: "Hean Prinsloo",
 					duration: "1:05:00",
 					type: "audio"
+				},
+				{
+					id: 15,
+					thumbnail: "/images/resume_themes/theme1000/media/9.png",
+					duration: "15:00",
+					type: "video"
+				},
+				{
+					id: 16,
+					thumbnail: "/images/resume_themes/theme1000/media/9.png",
+					duration: "15:00",
+					type: "video"
+				},
+				{
+					id: 17,
+					title: "AUDIO_04/08/2020",
+					thumbnail: "/images/resume_themes/theme1000/media/1.png",
+					artist: "Hean Prinsloo",
+					duration: "1:05:00",
+					type: "audio"
+				},
+				{
+					id: 18,
+					title: "AUDIO_04/08/2020",
+					thumbnail: "/images/resume_themes/theme1000/media/1.png",
+					artist: "Hean Prinsloo",
+					duration: "1:05:00",
+					type: "audio"
+				},
+				{
+					id: 19,
+					title: "AUDIO_04/08/2020",
+					thumbnail: "/images/resume_themes/theme1000/media/1.png",
+					artist: "Hean Prinsloo",
+					duration: "1:05:00",
+					type: "audio"
+				},
+				{
+					id: 20,
+					title: "AUDIO_04/08/2020",
+					thumbnail: "/images/resume_themes/theme1000/media/1.png",
+					artist: "Hean Prinsloo",
+					duration: "1:05:00",
+					type: "audio"
+				},
+				{
+					id: 21,
+					title: "AUDIO_04/08/2020",
+					thumbnail: "/images/resume_themes/theme1000/media/1.png",
+					artist: "Hean Prinsloo",
+					duration: "1:05:00",
+					type: "audio"
 				}
 			],
 			gridAreaValues: [],
+			desktopGridAreaValues: [],
 			windowWidth: window.innerWidth
 		};
 	},
@@ -140,6 +193,17 @@ export default {
 
 				if (this.windowWidth < 768) {
 					return "";
+				}
+
+				if (this.windowWidth >= 1920) {
+					let [
+						startRow,
+						startColumn,
+						endRow,
+						endColumn
+					] = this.desktopGridAreaValues[index];
+
+					return `grid-area: ${startRow} / ${startColumn} / ${endRow} / ${endColumn};`;
 				}
 
 				let [
@@ -191,11 +255,119 @@ export default {
 
 				return [startRow, startColumn, endRow, endColumn];
 			});
+		},
+		initDesktopGridAreas() {
+			const startRowIndex = 0,
+				endRowIndex = 2,
+				startColumnIndex = 1,
+				endColumnIndex = 3;
+
+			let startRow = 1,
+				endRow = 2,
+				startColumn = 1,
+				endColumn = 2;
+
+			// get simaple grids for 4 columns
+			let simpleGridAreas = this.media.map((item, index) => {
+				if (index === 0)
+					return [startRow, startColumn, endRow, endColumn];
+
+				startColumn++;
+				endColumn++;
+
+				if (startColumn === 5 && endColumn === 6) {
+					startColumn = 1;
+					endColumn = 2;
+
+					startRow++;
+					endRow++;
+				}
+
+				return [startRow, startColumn, endRow, endColumn];
+			});
+
+			// Give video items twice height of audio items
+			let desktopGridAreaValues = [];
+			simpleGridAreas.forEach((el, index) => {
+				let row = [...simpleGridAreas][index];
+
+				if (index === 0) {
+					desktopGridAreaValues.push(row);
+					return;
+				}
+
+				if (this.media[index].type === "video") row[endRowIndex]++;
+
+				if (
+					this.media[index - 1].type === "video" &&
+					this.media[index].type !== "video"
+				)
+					row[endRowIndex]--;
+
+				desktopGridAreaValues.push(row);
+			});
+
+			// Finally Give element the correct position
+			[...desktopGridAreaValues].forEach((el, index) => {
+				if (this.media[index].type === "video") {
+					for (
+						let i = index;
+						i < [...desktopGridAreaValues].length;
+						i++
+					) {
+						if (i - index === 4) {
+							for (
+								let j = i;
+								j < [...desktopGridAreaValues].length;
+								j += 4
+							) {
+								desktopGridAreaValues[j][startRowIndex]++;
+								desktopGridAreaValues[j][endRowIndex]++;
+							}
+						}
+					}
+				}
+			});
+
+			// Make sure last 4 items aligned correctly
+			let lastLineItems = [...desktopGridAreaValues]
+				.slice(desktopGridAreaValues.length - 4)
+				.filter(
+					el =>
+						el[0] ===
+						desktopGridAreaValues[
+							desktopGridAreaValues.length - 1
+						][0]
+				);
+
+			startColumn = 1;
+			endColumn = 2;
+
+			for (
+				let index = desktopGridAreaValues.length - lastLineItems.length,
+					i = 0;
+				index < desktopGridAreaValues.length;
+				index++, i++
+			) {
+				desktopGridAreaValues[index][startColumnIndex] = startColumn;
+				desktopGridAreaValues[index][endColumnIndex] = endColumn;
+
+				startColumn++;
+				endColumn++;
+			}
+
+			/*  if(lastLineItems.length<4) {
+            } */
+
+			// console.log(lastLineItems);
+
+			this.desktopGridAreaValues = desktopGridAreaValues;
 		}
 	},
 
 	mounted() {
 		this.initGridAreas();
+		this.initDesktopGridAreas();
 		window.onresize = e => (this.windowWidth = e.target.innerWidth);
 	}
 };
