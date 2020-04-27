90375671325176f7584a76549a3bbc96bb694f6f
Abdessamad El Hamdany <abdessamadelhamdany@gmail.com>
2020-04-27 15:02:31 +0000

    theme 1000 add simple about me to tab content

--- resources/js/components/resume_themes/theme1000/tabs/AboutMe.vue
+++ resources/js/components/resume_themes/theme1000/tabs/AboutMe.vue
@@ -1,6 +1,9 @@
 <template>
-	<div>
-		About me component
+	<div class="tw-px-4 tw-pt-16 tw-pb-12 md:tw-px-37px md:tw-max-w-4xl xxl:tw-max-w-1522px tw-text-black">
+		<h2 class="tw-font-poppins tw-font-medium tw-text-xl tw-leading-30px md:tw-text-30px md:tw-leading-civ-29">About me</h2>
+		<div class="tw-font-poppins tw-leading-4 tw-text-xs tw-pt-10px md:tw-pt-15px md:tw-text-lg md:tw-leading-7 xxl:tw-max-w-5xl">
+			Donec a augue gravida, vulputate ligula et, pellentesque arcu. Morbi feugiat eros nec sem ultrices, et venenatis velit posuere. Donec bibendum commodo dui, eget sollicitudin urna sagittis non. Donec ac commodo tortor. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris gravida laoreet lacus, non hendrerit elit suscipit a. Nunc ut ultricies massa, eu sollicitudin enim. Praesent quis ultrices nibh. Donec bibendum elit sed erat convallis, at feugiat arcu mollis. Nunc quam eros, venenatis id tristique malesuada, ornare eu augue. Aliquam volutpat eros id libero posuere vestibulum.
+		</div>
 	</div>
 </template>
 
