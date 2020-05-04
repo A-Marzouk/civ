f7489e5c155ca760ab5567e2a1671e3e177284b7
José Daniel  Quintero <josedanq100@gmail.com>
2020-05-04 17:49:33 -0400

    Renamed themes 9-109, 11-111, 12, 12-112, 13-113

--- resources/views/defaultThemes/theme13.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.theme-vuetify')
-
-@section('content')
-    <div id="resumeTheme13" class="d-flex justify-content-center">
-        <resume-theme-13  @if(isset($user)) :user="{{$user}}" @endif :is_preview="{{$is_preview}}"></resume-theme-13>
-    </div>
-@endsection
