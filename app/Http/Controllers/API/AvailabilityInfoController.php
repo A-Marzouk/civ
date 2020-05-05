569d0fc7bb54fa64166b8f11a6d4c9507998c12f
Ahmed-pc <perfect_ahmed@yahoo.com>
2020-05-05 11:15:57 +0300

    End point - avail. info

--- app/Http/Controllers/API/AvailabilityInfoController.php
+++ app/Http/Controllers/API/AvailabilityInfoController.php
@@ -37,6 +37,9 @@ class AvailabilityInfoController extends Controller
      */
     public function store(Request $request)
     {
+        if(!$this->is_auth($request)){
+            throw new Exception('Not Authenticated!');
+        }
         // here we will not save new we will directly update info.
         $this->validator($request->all())->validate();
 
@@ -55,8 +58,12 @@ class AvailabilityInfoController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'available_hours_frequency' => ['required', 'string','max:255','min:3'],
             'available_hours' => ['required', 'numeric','min:10','max:9999999']
         ]);
+    }
+
+
+    protected function is_auth($request){
+        return (Auth::user()->id == $request->user_id || Auth::user()->hasRole('admin'));
     }
 }
