196033b2d134406e37c3f8fe224a122c9b45b86a
Ahmed-pc <perfect_ahmed@yahoo.com>
2020-05-06 11:22:19 +0300

    End point - Links

--- resources/js/components/API/end_points/Links.vue
+++ resources/js/components/API/end_points/Links.vue
@@ -7,19 +7,22 @@
             </div>
             <div class="steps">
                 <div>
-                    Note: allowed methods: GET, POST, PUT & DELETE <small>(Examples included)</small>
+                    Note: allowed methods: GET, POST & DELETE <small>(Examples included)</small>
                 </div>
                 <div>
-                    Note: Please make sure to include current user_id in your request data when sending any POST or PUT request. <small>(Examples included)</small>
+                    Note: Please make sure to include current user_id in your request data when sending any POST request. <small>(Examples included)</small>
+                </div>
+                <div>
+                    <b>Note: Available Link Categories are : [ 'profile_link' , 'payment_link' , 'social_link' , 'portfolio_link' ]</b>
                 </div>
                 <div>
                     <div class="mb-2">
                         Fields of {{current_endpoint_single}} object :
                     </div>
                     <code class="p-4">
-                        Object_contains = [
-
-                        ];
+Object_contains = [
+    'category','link','link_title','is_active', 'user_id'
+];
                     </code>
                 </div>
             </div>
@@ -47,27 +50,64 @@
                         <div class="overline mb-4 mt-5"><b>Example response</b> | JSON</div>
 
                         <code class="prettyprint p-4 lang-js">
-                            {
-                            "data": {
-                            "id": 4,
-                            ....
-
-                            "user_id": 4,
-                            "created_at": "2020-05-03 12:38:43",
-                            "updated_at": "2020-05-03 12:38:43"
-                            },
-                            "version": "1.0.0",
-                            "author_url": "https://civ.ie"
-                            }
+{
+    "data": [
+        {
+            "id": 3,
+            "category": "profile_link",
+            "link": "http://agent.net",
+            "link_title": null,
+            "is_active": 1,
+            "user_id": 2,
+            "created_at": "2020-04-29 13:46:35",
+            "updated_at": "2020-05-06 08:09:11"
+        },
+        {
+            "id": 4,
+            "category": "payment_link",
+            "link": null,
+            "link_title": null,
+            "is_active": 1,
+            "user_id": 2,
+            "created_at": "2020-04-29 13:46:35",
+            "updated_at": "2020-04-29 13:46:35"
+        },
+        {
+            "id": 5,
+            "category": "Dolore quo dicta enim autem.",
+            "link": "http://parker.com/voluptatem-cupiditate-dolores-cumque-corporis-officia.html",
+            "link_title": "https://goodwin.com/at-deleniti-aut-repudiandae-iusto-nihil.html",
+            "is_active": 1,
+            "user_id": 2,
+            "created_at": "2020-04-29 13:46:35",
+            "updated_at": "2020-04-29 13:46:35"
+        }
+    ],
+    "links": {
+        "first": "http://localhost:8080/api/user/links?page=1",
+        "last": "http://localhost:8080/api/user/links?page=3",
+        "prev": null,
+        "next": "http://localhost:8080/api/user/links?page=2"
+    },
+    "meta": {
+        "current_page": 1,
+        "from": 1,
+        "last_page": 3,
+        "path": "http://localhost:8080/api/user/links",
+        "per_page": 5,
+        "to": 5,
+        "total": 13
+    }
+}
                         </code>
 
                         <div class="overline mb-4 mt-5"><b>Example ERROR response</b> | JSON</div>
 
 
                         <code class="prettyprint p-4 lang-js">
-                            {
-                            "message": "Unauthenticated."
-                            }
+{
+    "message": "Unauthenticated."
+}
                         </code>
 
 
@@ -88,98 +128,54 @@
                         Headers:
 
                         <code class="mb-5 prettyprint lang-js p-4">
-                            {{headers_after_auth}}
+{{headers_after_auth}}
                         </code>
 
                         Request data:
 
                         <code class="prettyprint p-4 mb-5 lang-js code">
-                            axios.post('/api/user/{{current_endpoint_url}}',
-                            {
-                            ....
-                            'user_id': 4
-                            }
-                            );
+axios.post('/api/user/{{current_endpoint_url}}',
+    {
+        category:'social_link',
+        link:'facebook.com/developer',
+        link_title: 'Facebook',
+        is_active: 1,
+        user_id: 4
+    }
+);
                         </code>
 
                         <div class="overline mb-4 mt-5"><b>Example response</b> | JSON</div>
 
                         <code class="prettyprint p-4 lang-js">
-                            {
-                            "data": {
-                            "id": 4,
-                            .....
-                            "created_at": "2020-05-03 12:38:43",
-                            "updated_at": "2020-05-04 13:24:11"
-                            },
-                            "version": "1.0.0",
-                            "author_url": "https://civ.ie"
-                            }
+{
+    "data": {
+        "category": "social_link",
+        "link": "facebook.com/ahmed",
+        "link_title": "facebook",
+        "is_active": "1",
+        "user_id": "2",
+        "updated_at": "2020-05-06 08:20:03",
+        "created_at": "2020-05-06 08:20:03",
+        "id": 24
+    },
+    "version": "1.0.0",
+    "author_url": "https://civ.ie"
+}
                         </code>
 
                         <div class="overline mb-4 mt-5"><b>Example ERROR response</b> | JSON</div>
 
 
                         <code class="prettyprint p-4 lang-js">
-                            {
-                            "message": "Not Authenticated!"
-                            }
-                        </code>
-
-
-                    </v-list-item-subtitle>
-                </v-list-item-content>
-            </v-list-item>
-        </v-card>
-
-        <hr>
-
-        <v-card class="mx-auto mt-5" max-width="95%" outlined>
-            <v-list-item three-line>
-                <v-list-item-content>
-                    <div class="overline mb-4">Update {{current_endpoint}} record | <b>Example request</b> | Javascript</div>
-                    <v-list-item-title class="headline mb-1 mb-3"> PUT: api/user/{{current_endpoint_url}} </v-list-item-title>
-                    <v-list-item-subtitle class="d-flex flex-column">
-
-                        Headers:
-
-                        <code class="mb-5 prettyprint lang-js p-4">
-                            {{headers_after_auth}}
-                        </code>
-
-                        Request data:
-
-                        <code class="prettyprint p-4 mb-5 lang-js code">
-                            axios.put('/api/user/{{current_endpoint_url}}',
-                            {
-                            ....
-                            'user_id': 4
-                            }
-                            );
-                        </code>
-
-                        <div class="overline mb-4 mt-5"><b>Example response</b> | JSON</div>
-
-                        <code class="prettyprint p-4 lang-js">
-                            {
-                            "data": {
-                            "id": 4,
-                            .....
-                            "created_at": "2020-05-03 12:38:43",
-                            "updated_at": "2020-05-04 13:24:11"
-                            },
-                            "version": "1.0.0",
-                            "author_url": "https://civ.ie"
-                            }
-                        </code>
-
-                        <div class="overline mb-4 mt-5"><b>Example ERROR response</b> | JSON</div>
-
-
-                        <code class="prettyprint p-4 lang-js">
-                            {
-                            "message": "Not Authenticated!"
-                            }
+{
+    "message": "The given data was invalid.",
+    "errors": {
+        "category": [
+          "The category field is required."
+        ]
+    }
+}
                         </code>
 
 
@@ -200,34 +196,32 @@
                         Headers:
 
                         <code class="mb-5 prettyprint lang-js p-4">
-                            {{headers_after_auth}}
+{{headers_after_auth}}
                         </code>
 
                         Request data:
 
                         <code class="prettyprint p-4 mb-5 lang-js code">
-                            axios.delete('/api/user/{{current_endpoint_url}}/' + {{current_endpoint_single}}.id)
+axios.delete('/api/user/{{current_endpoint_url}}/' + {{current_endpoint_single}}.id)
                         </code>
 
-                        <div class="overline mb-4 mt-5"><b>Example response</b> | JSON</div>
+                        <div class="overline mb-4 mt-5"><b>Example response</b> | JSON | ID of the deleted record</div>
 
                         <code class="prettyprint p-4 lang-js">
-                            {
-                            "data": {
-                            .....
-                            },
-                            "version": "1.0.0",
-                            "author_url": "https://civ.ie"
-                            }
+{
+    "data": {
+         "id": 3
+    }
+}
                         </code>
 
                         <div class="overline mb-4 mt-5"><b>Example ERROR response</b> | JSON</div>
 
 
                         <code class="prettyprint p-4 lang-js">
-                            {
-                            "message": "Not Authenticated!"
-                            }
+{
+    "message": "Not Authenticated!"
+}
                         </code>
 
 
