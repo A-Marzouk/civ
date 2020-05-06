8c96f04b7b708478daff578e117f61fb14dc74a5
Ahmed-pc <perfect_ahmed@yahoo.com>
2020-05-06 12:30:48 +0300

    End point - skills

--- resources/js/components/API/end_points/Skills.vue
+++ resources/js/components/API/end_points/Skills.vue
@@ -13,13 +13,16 @@
                     Note: Please make sure to include current user_id in your request data when sending any POST or PUT request. <small>(Examples included)</small>
                 </div>
                 <div>
+                    <b>Note: Allowed Skills Categories are : [ 'programming_languages' , 'frameworks' , 'design' , 'software' ]</b>
+                </div>
+                <div>
                     <div class="mb-2">
                         Fields of {{current_endpoint_single}} object :
                     </div>
                     <code class="p-4">
-                        Object_contains = [
-
-                        ];
+Object_contains = [
+    'title','category','percentage','user_id'
+];a
                     </code>
                 </div>
             </div>
@@ -35,39 +38,91 @@
                         Headers:
 
                         <code class="mb-5 prettyprint lang-js p-4">
-                            {{headers_after_auth}}
+{{headers_after_auth}}
                         </code>
 
                         Request example:
 
                         <code class="prettyprint p-4 mb-5 lang-js code">
-                            axios.get('/api/user/{{current_endpoint_url}}');
+axios.get('/api/user/{{current_endpoint_url}}');
                         </code>
 
                         <div class="overline mb-4 mt-5"><b>Example response</b> | JSON</div>
 
                         <code class="prettyprint p-4 lang-js">
-                            {
-                            "data": {
-                            "id": 4,
-                            ....
-
-                            "user_id": 4,
-                            "created_at": "2020-05-03 12:38:43",
-                            "updated_at": "2020-05-03 12:38:43"
-                            },
-                            "version": "1.0.0",
-                            "author_url": "https://civ.ie"
-                            }
+{
+    "data": [
+        {
+            "id": 1,
+            "title": "Libero nobis quos ut.",
+            "category": "programming_languages",
+            "percentage": 75,
+            "user_id": 2,
+            "created_at": "2020-04-29 13:46:35",
+            "updated_at": "2020-04-29 13:46:35"
+        },
+        {
+            "id": 2,
+            "title": "Voluptate non animi laborum.",
+            "category": "programming_languages",
+            "percentage": 70,
+            "user_id": 2,
+            "created_at": "2020-04-29 13:46:35",
+            "updated_at": "2020-04-29 13:46:35"
+        },
+        {
+            "id": 3,
+            "title": "Dignissimos eos eos ipsa eum.",
+            "category": "programming_languages",
+            "percentage": 64,
+            "user_id": 2,
+            "created_at": "2020-04-29 13:46:35",
+            "updated_at": "2020-04-29 13:46:35"
+        },
+        {
+            "id": 4,
+            "title": "Non recusandae ut voluptas.",
+            "category": "programming_languages",
+            "percentage": 72,
+            "user_id": 2,
+            "created_at": "2020-04-29 13:46:35",
+            "updated_at": "2020-04-29 13:46:35"
+        },
+        {
+            "id": 5,
+            "title": "Quaerat totam id optio non.",
+            "category": "programming_languages",
+            "percentage": 65,
+            "user_id": 2,
+            "created_at": "2020-04-29 13:46:35",
+            "updated_at": "2020-04-29 13:46:35"
+        }
+    ],
+    "links": {
+        "first": "http://localhost:8080/api/user/skills?page=1",
+        "last": "http://localhost:8080/api/user/skills?page=2",
+        "prev": null,
+        "next": "http://localhost:8080/api/user/skills?page=2"
+    },
+    "meta": {
+        "current_page": 1,
+        "from": 1,
+        "last_page": 2,
+        "path": "http://localhost:8080/api/user/skills",
+        "per_page": 5,
+        "to": 5,
+        "total": 10
+    }
+}
                         </code>
 
                         <div class="overline mb-4 mt-5"><b>Example ERROR response</b> | JSON</div>
 
 
                         <code class="prettyprint p-4 lang-js">
-                            {
-                            "message": "Unauthenticated."
-                            }
+{
+    "message": "Unauthenticated."
+}
                         </code>
 
 
@@ -88,42 +143,47 @@
                         Headers:
 
                         <code class="mb-5 prettyprint lang-js p-4">
-                            {{headers_after_auth}}
+{{headers_after_auth}}
                         </code>
 
                         Request data:
 
                         <code class="prettyprint p-4 mb-5 lang-js code">
-                            axios.post('/api/user/{{current_endpoint_url}}',
-                            {
-                            ....
-                            'user_id': 4
-                            }
-                            );
+axios.post('/api/user/{{current_endpoint_url}}',
+    {
+        title:'PHP',
+        category:'programming_languages',
+        percentage:85,
+        user_id: 4
+    }
+);
                         </code>
 
                         <div class="overline mb-4 mt-5"><b>Example response</b> | JSON</div>
 
                         <code class="prettyprint p-4 lang-js">
-                            {
-                            "data": {
-                            "id": 4,
-                            .....
-                            "created_at": "2020-05-03 12:38:43",
-                            "updated_at": "2020-05-04 13:24:11"
-                            },
-                            "version": "1.0.0",
-                            "author_url": "https://civ.ie"
-                            }
+{
+    "data": {
+        "user_id": "2",
+        "title": "PHP",
+        "category": "programming_languages",
+        "percentage": "85",
+        "updated_at": "2020-05-06 09:28:06",
+        "created_at": "2020-05-06 09:28:06",
+        "id": 11
+    },
+    "version": "1.0.0",
+    "author_url": "https://civ.ie"
+}
                         </code>
 
                         <div class="overline mb-4 mt-5"><b>Example ERROR response</b> | JSON</div>
 
 
                         <code class="prettyprint p-4 lang-js">
-                            {
-                            "message": "Not Authenticated!"
-                            }
+{
+    "message": "Not Authenticated!"
+}
                         </code>
 
 
@@ -144,42 +204,46 @@
                         Headers:
 
                         <code class="mb-5 prettyprint lang-js p-4">
-                            {{headers_after_auth}}
+{{headers_after_auth}}
                         </code>
 
                         Request data:
 
                         <code class="prettyprint p-4 mb-5 lang-js code">
-                            axios.put('/api/user/{{current_endpoint_url}}',
-                            {
-                            ....
-                            'user_id': 4
-                            }
-                            );
+axios.put('/api/user/{{current_endpoint_url}}',
+    {
+        id:11,
+        percentage:95,
+        user_id: 2
+    }
+);
                         </code>
 
                         <div class="overline mb-4 mt-5"><b>Example response</b> | JSON</div>
 
                         <code class="prettyprint p-4 lang-js">
-                            {
-                            "data": {
-                            "id": 4,
-                            .....
-                            "created_at": "2020-05-03 12:38:43",
-                            "updated_at": "2020-05-04 13:24:11"
-                            },
-                            "version": "1.0.0",
-                            "author_url": "https://civ.ie"
-                            }
+{
+    "data": {
+        "id": 11,
+        "title": "PHP",
+        "category": "programming_languages",
+        "percentage": "95",
+        "user_id": "2",
+        "created_at": "2020-05-06 09:28:06",
+        "updated_at": "2020-05-06 09:29:56"
+    },
+    "version": "1.0.0",
+    "author_url": "https://civ.ie"
+}
                         </code>
 
                         <div class="overline mb-4 mt-5"><b>Example ERROR response</b> | JSON</div>
 
 
                         <code class="prettyprint p-4 lang-js">
-                            {
-                            "message": "Not Authenticated!"
-                            }
+{
+    "message": "Not Authenticated!"
+}
                         </code>
 
 
@@ -200,34 +264,32 @@
                         Headers:
 
                         <code class="mb-5 prettyprint lang-js p-4">
-                            {{headers_after_auth}}
+{{headers_after_auth}}
                         </code>
 
                         Request data:
 
                         <code class="prettyprint p-4 mb-5 lang-js code">
-                            axios.delete('/api/user/{{current_endpoint_url}}/' + {{current_endpoint_single}}.id)
+axios.delete('/api/user/{{current_endpoint_url}}/' + {{current_endpoint_single}}.id)
                         </code>
 
-                        <div class="overline mb-4 mt-5"><b>Example response</b> | JSON</div>
+                        <div class="overline mb-4 mt-5"><b>Example response</b> | JSON | ID of the deleted record</div>
 
                         <code class="prettyprint p-4 lang-js">
-                            {
-                            "data": {
-                            .....
-                            },
-                            "version": "1.0.0",
-                            "author_url": "https://civ.ie"
-                            }
+{
+    "data": {
+        "id": 5
+    }
+}
                         </code>
 
                         <div class="overline mb-4 mt-5"><b>Example ERROR response</b> | JSON</div>
 
 
                         <code class="prettyprint p-4 lang-js">
-                            {
-                            "message": "Not Authenticated!"
-                            }
+{
+    "message": "Not Authenticated!"
+}
                         </code>
 
 
@@ -235,7 +297,6 @@
                 </v-list-item-content>
             </v-list-item>
         </v-card>
-
 
     </div>
 </template>
