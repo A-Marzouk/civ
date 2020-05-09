1182af56a9d326ccd5a1cf5358a24d5e8191161e
Nishad Islam <nishad.bdg@gmail.com>
2020-05-09 12:09:30 +0600

    welcome landing page autoplay added to the sliders

--- resources/js/components/welcome-view2/welcome.vue
+++ resources/js/components/welcome-view2/welcome.vue
@@ -403,6 +403,8 @@ export default {
         arrows: false,
         slidesToShow: 3,
         slidesToScroll: 1,
+        autoplay:true,
+        autoplaySpeed:5000,
         responsive: [
           {
             breakpoint: 959,
