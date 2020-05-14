d42bbb4e4df5c8ff87482eebd2c307656f275256
Ahmed-pc <perfect_ahmed@yahoo.com>
2020-05-14 09:43:39 +0300

    Add theme tab

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -13,13 +13,15 @@ import adminDashboard from './components/admin/Dashboard';
 import General from './components/admin/tabs/General';
 import accountSettings from './components/admin/tabs/Settings';
 import Users from './components/admin/tabs/UsersTable';
+import Themes from './components/admin/tabs/Themes';
 
 if ($("#adminDashboard").length !== 0) {
 
     const routes = [
         {path: '/', component: General, name:'general'},
         {path: '/account-settings', component: accountSettings, name:'acc_settings'},
-        {path: '/users', component: Users, name:'users'}
+        {path: '/users', component: Users, name:'users'},
+        {path: '/themes', component: Themes, name:'themes'}
     ];
 
     const router = new VueRouter({
