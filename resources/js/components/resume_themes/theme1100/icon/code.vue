29c806bcae4ae41231f5649f1a7eaa332aead977
jubayer11 <ahmedjubayer54@gmail.com>
2020-05-15 03:28:20 +0600

    revise theme 1100 and 1101

---
+++ resources/js/components/resume_themes/theme1100/icon/code.vue
@@ -0,0 +1,33 @@
+<template>
+    <svg id="Mask_Group_3" data-name="Mask Group 3" xmlns="http://www.w3.org/2000/svg" width="33.94" height="40"
+         viewBox="0 0 38.94 45">
+        <metadata><?xpacket begin="﻿" id="W5M0MpCehiHzreSzNTczkc9d"?>
+            <x:xmpmeta xmlns:x="adobe:ns:meta/"
+                       x:xmptk="Adobe XMP Core 5.6-c138 79.159824, 2016/09/14-01:09:01        ">
+                <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+                    <rdf:Description rdf:about=""/>
+                </rdf:RDF>
+            </x:xmpmeta>
+
+
+            <?xpacket end="w"?></metadata>
+        <defs>
+
+        </defs>
+        <g id="surface1">
+            <path id="Path_13" data-name="Path 13" class="cls-1"
+                  d="M4169.5,407l-19.48,11.25v22.5L4169.5,452l19.48-11.25v-22.5Zm-7.52,26.046-1.87,1.866-5.41-5.412,5.41-5.412,1.87,1.865-3.55,3.547Zm5.59,3.254-2.36-1.18,6.22-12.421,2.35,1.18Zm11.32-1.388-1.87-1.866,3.55-3.546-3.55-3.547,1.87-1.866,5.41,5.413Z"
+                  transform="translate(-4150.03 -407)"/>
+        </g>
+    </svg>
+
+
+</template>
+<style scoped>
+
+    .cls-1 {
+        fill: #2f3458;
+        fill-rule: evenodd;
+    }
+
+</style>
---
+++ resources/views/defaultThemes/theme1100.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.theme-vuetify')
+
+@section('content')
+    <div style="font-family: 'Cabin', sans-serif" id="resumeTheme1100">
+        <resume-theme-1100 page="resume"></resume-theme-1100>
+    </div>
+@endsection
