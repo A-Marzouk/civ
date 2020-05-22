f6db52bc096bf10df13f986aa126e7c7543e6990
Abdessamad El Hamdany <abdessamadelhamdany@gmail.com>
2020-05-22 11:49:40 +0000

    theme 1002 setup

---
+++ resources/js/components/resume_themes/theme1002/Index.vue
@@ -0,0 +1,20 @@
+<template>
+	<div id="wrapper_theme1002">
+		Theme 1002
+	</div>
+</template>
+
+<script>
+export default {
+	name: "resume-theme-1002",
+
+	components: {},
+
+	data: () => {
+		return {
+			currentTab: "portfolio",
+			currentUser: {}
+		};
+	}
+};
+</script>
