748dfa2381bb56c760225c828abea8401550dc6b
Nishad Islam <nishad.bdg@gmail.com>
2020-05-26 22:26:28 +0600

    pricing page updated for tablet version

--- resources/js/components/welcome-view/pricing.vue
+++ resources/js/components/welcome-view/pricing.vue
@@ -3,7 +3,7 @@
     <!-- main container starts here -->
     <v-container class="main-container">
       <v-row align="center" justify="center">
-        <v-col xl="11" lg="11" md="12">
+        <v-col xl="11" lg="11" md="11" sm="11" cols="11">
           <v-app-bar color="transparent" flat tile height="100">
             <div class="div-back-btn">
               <v-btn fab color="#0046FE" depressed dark class="btn-back">
@@ -15,16 +15,17 @@
             <a href="/" class="link-back2">Home</a>
           </v-app-bar>
           <v-container style="width: 100%;">
-            <v-row align="center" justify="center">
-              <v-col cols="12">
+            <!-- For Desktop -->
+            <v-row align="center" justify="center" >
+              <v-col xl="12" lg="12">
                 <v-card flat tile color="transparent" align="center">
                   <v-card-subtitle class="choose-plan-text">Choose Your Plan</v-card-subtitle>
                 </v-card>
               </v-col>
-              <v-col md="4" sm="12" cols="12" v-for="plan in plans" :key="plan.id">
+              <v-col md="4" sm="12" cols="12" v-for="plan in plans" :key="plan.id" class="hidden-md-and-down">
                 <v-card
                   class="price-card mt-10 pa-5"
-                  :class ="plan.title == 'Standard'?'box-shadow-standard': 'box-shadow-regular'"
+                  :class="plan.title == 'Standard'?'box-shadow-standard': 'box-shadow-regular'"
                 >
                   <v-card-subtitle class="price-title" align="center">{{plan.title}}</v-card-subtitle>
                   <v-card-subtitle
@@ -59,6 +60,68 @@
                 </v-card>
               </v-col>
             </v-row>
+            <!-- For Desktop -->
+            <!-- For tablet and mobile -->
+            <v-row class="hidden-lg-and-up" align="center" justify="center">
+              <v-col md="12" sm="12" cols="12">
+                <v-card color="transparent" flat tile>
+                  <v-tabs centered hide-slider center-active grow v-model="price_tab">
+                    <v-tab
+                      v-for="plan in plans"
+                      :key="plan.id"
+                      @click="currentTab=plan.id"
+                      class="mx-3"
+                      :class="[
+                        currentTab == plan.id ? 'custom-active-tab' : '', 'ct-tab',
+                      ]"
+                    >{{plan.title}}</v-tab>
+                  </v-tabs>
+                </v-card>
+              </v-col>
+              <v-col md="12" sm="12" cols="12">
+                <v-card color="transparent" flat tile>
+                  <v-tabs-items v-model="price_tab">
+                    <v-tab-item v-for="plan in plans" :key="plan.id" align="center">
+                      <v-card
+                        class="price-card mt-10 pa-5 box-shadow-standard"
+                      >
+                        <v-card-subtitle class="price-title" align="center">{{plan.title}}</v-card-subtitle>
+                        <v-card-subtitle
+                          align="center"
+                          :class="plan.title == 'Free' ? 'mt-n10': 'subtitle-pro mt-n10' "
+                        >
+                          <span class="usd">
+                            <sup>$</sup>
+                            <span class="amount">{{plan.price}}</span>
+                          </span>
+
+                          <span class="usd ml-2">{{plan.frequency}}</span>
+                        </v-card-subtitle>
+
+                        <v-card-subtitle align="center">
+                          <v-btn
+                            v-show="plan.title == 'Free'"
+                            outlined
+                            class="btn-free-sub"
+                          >{{plan.btn_title}}</v-btn>
+                          <v-btn
+                            color="#0046FE"
+                            v-show="plan.title != 'Free'"
+                            class="btn-pro"
+                          >{{plan.btn_title}}</v-btn>
+                        </v-card-subtitle>
+                        <v-card-text>
+                          <ul class="features mt-10" align="left">
+                            <li v-for="(feature,index) in plan.features" :key="index">{{feature}}</li>
+                          </ul>
+                        </v-card-text>
+                      </v-card>
+                    </v-tab-item>
+                  </v-tabs-items>
+                </v-card>
+              </v-col>
+            </v-row>
+            <!-- for tablet and mobile -->
           </v-container>
         </v-col>
       </v-row>
@@ -71,6 +134,8 @@
 export default {
   data() {
     return {
+      price_tab: 1,
+      currentTab: 1,
       plans: [
         {
           id: 1,
@@ -162,15 +227,20 @@ $line-height55: 55px;
     font-size: 64px;
     line-height: $line-height55;
     color: #0046fe !important;
+    @media screen and (min-width: 600px) and (max-width: 1263px){
+      font-size: 36px;
+    }
+    @media screen and (max-width: 599px){
+      font-size: 24px;
+    }
   }
 
-  .box-shadow-regular{
+  .box-shadow-regular {
     box-shadow: 0px 4px 40px rgba(0, 70, 254, 0.08);
   }
-  .box-shadow-standard{
+  .box-shadow-standard {
     box-shadow: 0px 4px 40px rgba(0, 70, 254, 0.15);
   }
-
 
   .price-card {
     border-radius: 15px !important;
@@ -178,6 +248,10 @@ $line-height55: 55px;
     height: 600px !important;
     @media screen and (min-width: 1264px) and (max-width: 1903px) {
       height: 540px !important;
+    }
+
+    @media screen and (min-width: 600px) and (max-width: 1263px) {
+      height: 608px !important;
     }
 
     .price-title {
@@ -188,6 +262,9 @@ $line-height55: 55px;
       color: #0a1e56 !important;
       @media screen and (min-width: 1264px) and (max-width: 1903px) {
         font-size: 30px;
+      }
+      @media screen and (min-width: 600px) and (max-width: 1263px) {
+        font-size: 36px;
       }
     }
     .subtitle-pro {
@@ -208,6 +285,9 @@ $line-height55: 55px;
       @media screen and (min-width: 1264px) and (max-width: 1903px) {
         font-size: 14px;
       }
+      @media screen and (min-width: 600px) and (max-width: 1263px) {
+        font-size: 18px;
+      }
     }
     .amount {
       font-family: "Montserrat" !important;
@@ -217,6 +297,9 @@ $line-height55: 55px;
       color: #0a1e56 !important;
       @media screen and (min-width: 1264px) and (max-width: 1903px) {
         font-size: 30px;
+      }
+      @media screen and (min-width: 600px) and (max-width: 1263px) {
+        font-size: 36px;
       }
     }
 
@@ -239,6 +322,12 @@ $line-height55: 55px;
         width: 250px !important;
         font-size: 10px;
       }
+
+      @media screen and (min-width: 600px) and (max-width: 1263px) {
+        width: 270px !important;
+        height: 59.65px !important;
+        font-size: 12px;
+      }
     }
     .btn-pro {
       width: 270px !important;
@@ -258,6 +347,11 @@ $line-height55: 55px;
         width: 250px !important;
         font-size: 10px;
       }
+      @media screen and (min-width: 600px) and (max-width: 1263px) {
+        width: 270px !important;
+        height: 59.65px !important;
+        font-size: 12px;
+      }
     }
 
     ul {
@@ -266,6 +360,12 @@ $line-height55: 55px;
       margin-top: 10px;
       @media screen and (min-width: 1264px) and (max-width: 1903px) {
         margin-left: 1.5vw;
+      }
+      @media screen and (min-width: 960px) and (max-width: 1263px){
+        margin-left: 8vw;
+      }
+      @media screen and (min-width: 600px) and (max-width: 959px){
+        margin-left: 12vw;
       }
     }
     ul li::before {
@@ -287,10 +387,39 @@ $line-height55: 55px;
         font-size: 16px;
         line-height: 30px;
       }
+      @media screen and (min-width: 600px) and (max-width: 1263px) {
+        font-size: 18px;
+      }
+    }
+  } //price card ends here
+  .price-card:hover {
+    box-shadow: 0px 4px 40px rgba(0, 70, 254, 0.2);
+  }
+
+  .ct-tab {
+    width: 180px;
+    font-family: "Montserrat" !important;
+    font-weight: bold;
+    font-size: 24px !important;
+    line-height: $line-height55;
+    color: rgba(10, 30, 86, 0.4) !important;
+    text-transform: capitalize;
+    border-radius: 15px 15px 0px 0px;
+    box-shadow: 0px 4px 40px rgba(0, 70, 254, 0.15);
+    @media screen and (max-width: 599px){
+      width: 110px;
+      font-size: 12px !important;
     }
   }
-  .price-card:hover{
-    box-shadow: 0px 4px 40px rgba(0, 70, 254, 0.2);
+  .custom-active-tab {
+    font-size: 30px !important;
+    background-color: #0046fe !important;
+    color: #ffffff !important;
+    border-radius: 15px 15px 0px 0px;
+    @media screen and (max-width: 599px){
+      width: 135px !important;
+      font-size: 18px !important;
+    }
   }
 }
 </style>
