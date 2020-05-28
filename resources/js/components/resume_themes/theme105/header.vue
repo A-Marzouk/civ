7f071a0f5b8c9aee46c2ee5c1948fd50c1d57154
José Daniel  Quintero <josedanq100@gmail.com>
2020-05-28 12:25:12 -0400

    fixed header theme105

--- resources/js/components/resume_themes/theme105/header.vue
+++ resources/js/components/resume_themes/theme105/header.vue
@@ -360,7 +360,14 @@ $purple: #355E5E;
                 }
             }
 
+            @media (max-width: 1200px) {
+                margin-top: 27px;
+            }
+
             @media (max-width: 700px) {
+                margin-top: 0;
+                padding-left: 60px;
+                justify-content: space-between;
                 width: 100%;
             }
         }
