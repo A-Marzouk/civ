6f3a235a98739d5e76589d91b88aafc84d8a0717
José Daniel  Quintero <josedanq100@gmail.com>
2020-06-08 13:07:28 -0400

    Starting new education view

---
+++ resources/js/components/resume_builder/tabs/edit_tabs/education_tabs/school.vue
@@ -0,0 +1,111 @@
+<template>
+    <div
+        class="education-wrapper"
+    >
+        <v-form
+            ref="form"
+            :lazy-validation="lazy"
+        >
+            <v-container>
+                <v-row>
+                    <v-col>
+                        <v-row>
+                            <!-- College Name -->
+                            <v-text-field
+                                id="collegeName"
+                                v-model="form.collegeName.value"
+                                class="resume-builder__input civie-input"
+                                outlined
+                                color="#001CE2"
+                                :rules="form.collegeName.rules"
+                                :class="{'resume-builder__input--disabled': form.collegeName.disabled}"
+                                :disabled="form.collegeName.disabled"
+                            >
+                                <button
+                                    v-if="inputProps.showToggleInputIcon"
+                                    class="eye-icon trigger-icon icon"
+                                    :class="{'icon--disabled': disabledInput}"
+                                    slot="append"
+                                    @click="toggleInput(form.collegeName)" 
+                                >
+                                    <svg-vue
+                                        :icon="`eye-icon`" 
+                                    ></svg-vue>
+                                </button>
+                            </v-text-field>
+                        </v-row>
+                        <v-row>
+
+                        </v-row>  
+                    </v-col>
+                    <v-col>
+                        <v-row>
+                        </v-row>
+                        <v-row>
+                        </v-row> 
+                    </v-col>
+                    <v-col>
+                        <v-row>
+                        </v-row>
+                        <v-row>
+                        </v-row> 
+                    </v-col>
+                    <v-col>
+                        
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-form>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        form: {
+            collegeName: {
+                value: '',
+                disabled: false,
+                rules: [
+                    value => !!value || 'Please fill this field.',
+                ]
+            },
+            website: {}
+        },
+        educationList: [
+            {
+                id: 0,                
+                schoolName: "Simon Bolívar University",
+                location: "Caracas, Venezuela",
+                gradeTitle: "Lic. Mathematics: Computing and Statistics",
+                startDate: "2014",
+                actualStudying: false,
+                endDate: "2019"
+            },
+            {
+                id: 1,                
+                schoolName: "Simon Bolívar University",
+                location: "Caracas, Venezuela",
+                gradeTitle: "Lic. Mathematics: Computing and Statistics",
+                startDate: "2014",
+                actualStudying: false,
+                endDate: "2019"
+            },
+            {
+                id: 2, 
+                schoolName: "Simon Bolívar University",
+                location: "Caracas, Venezuela",
+                gradeTitle: "Lic. Mathematics: Computing and Statistics",
+                startDate: "2014",
+                actualStudying: false,
+                endDate: "2019"
+            }
+        ]
+    }),
+    methods: {
+        toggleInput(inputData) {
+            inputData.disabled = !inputData.disabled
+        }
+    }
+}
+</script>
