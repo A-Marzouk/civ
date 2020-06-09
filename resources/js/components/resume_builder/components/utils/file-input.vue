2d3ec515ccf9c2e1f2f5b4dfb569438ddd0bf39b
José Daniel  Quintero <josedanq100@gmail.com>
2020-06-09 16:31:15 -0400

    dropzone

---
+++ resources/js/components/resume_builder/components/utils/file-input.vue
@@ -0,0 +1,27 @@
+<template>
+    <vue-dropzone class="civie-dropzone" ref="myVueDropzone" id="dropzone" :options="dropzoneOptions" :useCustomSlot=true>
+      <div class="dropzone-custom-content">
+        <svg-vue class="icon" :icon="'upload-input-icon'"></svg-vue>
+      </div>
+    </vue-dropzone>
+</template>
+
+<script>
+import vue2Dropzone from 'vue2-dropzone'
+import 'vue2-dropzone/dist/vue2Dropzone.min.css'
+export default {
+  name: 'app',
+  components: {
+    vueDropzone: vue2Dropzone
+  },
+  data: function () {
+    return {
+      dropzoneOptions: {
+          url: 'https://httpbin.org/post',
+          thumbnailWidth: 150,
+          maxFilesize: 0.5,
+      }
+    }
+  }
+}
+</script>
