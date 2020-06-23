8ff2aa7ac89fa87ee88af34a86c1d11b5fae0727
Abdessamad El Hamdany <abdessamadelhamdany@gmail.com>
2020-06-23 13:46:02 +0100

    theme2 add media modals structure

---
+++ resources/js/components/resume_themes/theme2/components/AudioMedia.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="audio-media">
+		<a class="open-audio-media" href="#" @click.prevent="open">
+			<img src="/images/resume_themes/theme2/sound.svg" alt="sound icon">
+		</a>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "audio-media"
+};
+</script>
+
+<style lang="scss" scoped>
+.audio-media {
+	.open-audio-media {
+		margin-bottom: 14px;
+
+		img {
+			width: 48px;
+			height: 48px;
+		}
+	}
+}
+</style>
