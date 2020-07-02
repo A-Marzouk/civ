e32b21a455e6d3c702752a44f18d407df9549213
José Daniel  Quintero <josedanq100@gmail.com>
2020-07-02 19:36:12 -0400

    feat: default config for theme113 pdf preview

---
+++ resources/views/defaultPDFThemes/theme113.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <style>
+
+        @font-face {
+            font-family: 'Muli';
+            src: url("/fonts/Muli-VariableFont_wght.ttf") format("truetype");
+            /* font-weight: 400; // use the matching font-weight here ( 100, 200, 300, 400, etc).
+            font-style: normal; // use the matching font-style here */
+        }
+
+        main {
+            background-image: url('/images/resume_themes/theme113/mainBg.png');
+            background-color: #FFF7F3;
+            background-position: -10px 300px;
+            height: 200px;
+            font-family: Muli, Arial, Helvetica, sans-serif;
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <main>
+        <div class="theme-header">
+            Theme103
+        </div>
+        <div class="theme-body">
+
+        </div>
+    </main>
+</body>
+</html>
