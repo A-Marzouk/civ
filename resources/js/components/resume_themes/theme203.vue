6e7ee6a40cd35f0d6387e9a47d94eec4ee01c665
Nishad Islam <nishad.bdg@gmail.com>
2020-07-13 23:59:27 +0600

    achievement and portfolio tab horizontal bar removed

--- resources/js/components/resume_themes/theme203.vue
+++ resources/js/components/resume_themes/theme203.vue
@@ -585,7 +585,7 @@ export default {
           icon_text: "fig"
         }
       ],
-      user: this.currentUser,
+      currentUser: this.user
     };
   },
   computed: {
@@ -628,97 +628,6 @@ export default {
       });
 
       return providerLink;
-    },
-    getSkillIcon(skill_title) {
-      let arrayOfSkillImages = {
-        "ui design": "/images/skills_icons/user_interface.png",
-        "ux design": "/images/skills_icons/user_experience.png",
-        "logo design": "/images/skills_icons/logo_design.png",
-        animation: "/images/skills_icons/animation.jpg",
-        "motion graphics": "/images/skills_icons/motion_graphics.png",
-        illustration: "/images/skills_icons/illustration.png",
-        advertising: "/images/skills_icons/advertising.png",
-        branding: "/images/skills_icons/branding.png",
-        "brochure Design": "/images/skills_icons/brochure_design.png",
-        "website design": "/images/skills_icons/web_design.png",
-        "game designer": "/images/skills_icons/game_designer.png",
-        "character design": "/images/skills_icons/character_design.png",
-        "digital painting": "/images/skills_icons/digital_painting.png",
-        "creative director": "/images/skills_icons/creative_director.png",
-        "html / css": "/images/skills_icons/HTML.png",
-        // 2-
-
-        "adobe after effects": "/images/skills_icons/AE.png",
-        sketch: "/images/skills_icons/Sketch.png",
-        "adobe illustrator": "/images/skills_icons/Illustrator.png",
-        "adobe xd": "/images/skills_icons/AdobeXD.png",
-        photoshop: "/images/skills_icons/Photoshop.png",
-        autocad: "/images/skills_icons/autocad.png",
-        solidworks: "/images/skills_icons/solid_works.png",
-        "adobe flash": "/images/skills_icons/adobe_flash.png",
-        "digital drawing Tablet":
-          "/images/skills_icons/digital_drawing_tablet.png",
-        "adobe indesign": "/images/skills_icons/indesign.png",
-        coreldraw: "/images/skills_icons/corel_draw.png",
-        "3d max": "/images/skills_icons/3d_max.png",
-
-        // developer :
-        // 1-
-        javascript: "/images/skills_icons/javascript.png",
-        sql: "/images/skills_icons/mysql.png",
-        java: "resumeApp/resources/assets/images/skills_icons/java.png",
-        "c#": "/images/skills_icons/c#.png",
-        python: "/images/skills_icons/python.png",
-        php: "/images/skills_icons/php.png",
-        "c++": "/images/skills_icons/c_language.png",
-        c: "/images/skills_icons/c_language.png",
-        typescript: "/images/skills_icons/typescript.png",
-        ruby: "/images/skills_icons/ruby.png",
-        "objective-C": "/images/skills_icons/objective_c.png",
-        swift: "/images/skills_icons/swift.png",
-        "vb.net": "/images/skills_icons/vb_net.png",
-        go: "/images/skills_icons/go.png",
-        perl: "/images/skills_icons/perl.png",
-        scala: "/images/skills_icons/scala.png",
-        groovy: "/images/skills_icons/groovy.png",
-        assembly: "/images/skills_icons/assembly.png",
-        coffeescript: "/images/skills_icons/coffeeScript.png",
-        vba: "/images/skills_icons/vba.png",
-        r: "/images/skills_icons/r_lang.png",
-        matlab: "/images/skills_icons/matlab.png",
-        "visual basic 6": "/images/skills_icons/matlab.png",
-        lua: "/images/skills_icons/lua.png",
-        haskell: "/images/skills_icons/haskell.png",
-        html: "/images/skills_icons/HTML.png",
-        css: "/images/skills_icons/CSS.png",
-        laravel: "/images/skills_icons/laravel.png",
-        phpstorm: "/images/skills_icons/phpstorm.png",
-
-        //2-
-        angularjs: "/images/skills_icons/Angularjs.png",
-        "angular.js": "/images/skills_icons/Angularjs.png",
-        "node.js": "/images/skills_icons/node_js.png",
-        nodejs: "/images/skills_icons/node_js.png",
-        ".net Core": "/images/skills_icons/netcore.png",
-        react: "/images/skills_icons/react.png",
-        cordova: "/images/skills_icons/cordava.png",
-        firebase: "",
-        xamarin: "",
-        hadoop: "/images/skills_icons/hadoop.png",
-        spark: "/images/skills_icons/spark.png",
-        mysql: "/images/skills_icons/mysql.png",
-        "sql server": "/images/skills_icons/sql server.png",
-        postgresql: "/images/skills_icons/postgreSQL.png",
-        sqlite: "/images/skills_icons/SQLite.png",
-        mongodb: "/images/skills_icons/mongoDB.png",
-        oracle: "/images/skills_icons/Oracle.png",
-        redis: "/images/skills_icons/redis.png",
-        cassandra: "/images/skills_icons/cassandra.png"
-      };
-      if (arrayOfSkillImages.hasOwnProperty(skill_title.toLowerCase())) {
-        return arrayOfSkillImages[skill_title.toLowerCase()];
-      }
-      return "/images/skills_icons/skill.png";
     },
     sendEmail() {},
     setDummyUser() {
