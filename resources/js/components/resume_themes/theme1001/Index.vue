ba7008b79555f3e1efee0a2f2aeac3663408872a
neutral-Stage <anirbanroy691@gmail.com>
2020-07-17 11:49:41 +0600

    added Media section in theme1001

--- resources/js/components/resume_themes/theme1001/Index.vue
+++ resources/js/components/resume_themes/theme1001/Index.vue
@@ -1,11 +1,14 @@
 <template>
-	<div id="wrapper_theme1001">
-		<Header :currentTab="currentTab" :currentUser="currentUser" />
+  <div id="wrapper_theme1001">
+    <Header :currentTab="currentTab" :currentUser="currentUser" />
 
-		<TabsNavigation :currentTab="currentTab" @tabChanged="currentTab=$event" />
+    <TabsNavigation
+      :currentTab="currentTab"
+      @tabChanged="currentTab = $event"
+    />
 
-		<TabsContent :currentTab="currentTab" />
-	</div>
+    <TabsContent :currentTab="currentTab" />
+  </div>
 </template>
 
 <script>
@@ -14,21 +17,21 @@ import TabsNavigation from "./components/TabsNavigation";
 import TabsContent from "./components/TabsContent";
 
 export default {
-	name: "resume-theme-1001",
+  name: "resume-theme-1001",
 
-	components: { Header, TabsNavigation, TabsContent },
+  components: { Header, TabsNavigation, TabsContent },
 
-	data: () => {
-		return {
-			currentTab: "portfolio",
-			currentUser: {
-				avatar: "/images/resume_themes/theme1001/profiles/girl.jpg",
-				fullname: "Amber Salma Heard",
-				jobTitle: "UX UI Designer",
-				hourRate: 45,
-				weeklyAvailability: 45
-			}
-		};
-	}
+  data: () => {
+    return {
+      currentTab: "portfolio",
+      currentUser: {
+        avatar: "/images/resume_themes/theme1001/profiles/girl.jpg",
+        fullname: "Amber Salma Heard",
+        jobTitle: "UX UI Designer",
+        hourRate: 45,
+        weeklyAvailability: 45
+      }
+    };
+  }
 };
 </script>
---
+++ resources/js/components/resume_themes/theme1001/components/Media/Video.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-card class="video-tm903">
+    <div ref="videoPoster">
+      <v-img class="img-video-tm903" cover :src="img" alt>
+        <v-btn
+          class="b-play-tm903"
+          color="#D4DDFF"
+          height="90"
+          width="90"
+          style="border:1px solid #3D5ED4;"
+          dark
+          fab
+          elevation="0"
+          @click.native="playVideo()"
+        >
+          <v-icon color="#3D5ED4" large>mdi-play</v-icon>
+        </v-btn>
+      </v-img>
+    </div>
+    <div ref="videotm903" class="videotm903">
+      <v-icon @click.prevent="stopVideo()" right>mdi-close</v-icon>
+      <video width="100%" ref="videoElem" controls :src="link"></video>
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    link: {
+      type: String,
+      default: null
+    },
+    img: {
+      type: String,
+      default: null
+    }
+  },
+  methods: {
+    playVideo() {
+      this.$refs.videotm903.style.display = "block";
+      this.$refs.videoPoster.style.display = "none";
+      this.$refs.videoElem.play();
+    },
+    stopVideo() {
+      this.$refs.videotm903.style.display = "none";
+      this.$refs.videoPoster.style.display = "block";
+      this.$refs.videoElem.pause();
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.video-tm903 {
+  box-shadow: 0px 6px 6px rgba(0, 0, 0, 0.15);
+  border-radius: 10px;
+
+  .b-play-tm903 {
+    left: -20%;
+    top: 38%;
+    @media only screen and (max-width: 597px) {
+      left: -20%;
+      top: 38%;
+    }
+  }
+  .videotm903 {
+    display: none;
+  }
+}
+</style>
