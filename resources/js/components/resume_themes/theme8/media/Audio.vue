6680db5daaca548ceb0165929f2a253cdb5cdb09
Nishad Islam <nishad.bdg@gmail.com>
2020-07-29 16:11:09 +0600

    theme 8 media modal added

---
+++ resources/js/components/resume_themes/theme8/media/Audio.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="media-content-audio">
+      <VueSlickCarousel :dots="true">
+          <div v-for="i in 6" :key="i">{{i}}</div>
+      </VueSlickCarousel>
+  </div>
+</template>
+<script>
+import VueSlickCarousel from "vue-slick-carousel";
+
+export default {
+  components: {
+      VueSlickCarousel,
+  },
+
+};
+</script>
