af3575fe22abb1f2821e63550544fcb32cee1811
Nishad Islam <nishad.bdg@gmail.com>
2020-08-26 22:45:01 +0600

    reset password template updated

--- resources/views/emails/reset-password.blade.php
+++ resources/views/emails/reset-password.blade.php
@@ -1,10 +1,9 @@
 @component('mail::message')
-# Introduction 
+# Introduction
 ## Hi John,
 
 You recently requested a link to reset your civ.ie password.
 Please set a new password by the link below:
-
 
 @component('mail::button', ['url' => ''])
 Change Password
@@ -15,5 +14,22 @@ Kindest regards,<br>
 civ.ie team
 <!-- {{ config('app.name') }} -->
 @endcomponent
+
 <style>
+    body {
+        color: #888DB1 !important;
+    }
+
+    body>table>tbody>tr>td>table>tbody>tr:nth-child(2)>td>table>tbody>tr>td>table>tbody>tr>td>table>tbody>tr>td>table {
+        float: left !important;
+    }
+
+    .button .button-primary {
+        background-color: #0046FE !important;
+        border-bottom: 8px solid #0046FE !important;
+        border-left: 18px solid #0046FE !important;
+        border-right: 18px solid #0046FE !important;
+        border-top: 8px solid #0046FE !important;
+        border-radius: 5px !important;
+    }
 </style>
