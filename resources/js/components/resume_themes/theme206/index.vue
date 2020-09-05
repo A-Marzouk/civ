b2e30f8e3023e6f6435ffd2748149fa90e91ad70
Nishad Islam <nishad.bdg@gmail.com>
2020-09-05 13:38:48 +0600

    theme 206 tablet version update

--- resources/js/components/resume_themes/theme206/index.vue
+++ resources/js/components/resume_themes/theme206/index.vue
@@ -90,7 +90,7 @@
                 <!-- Hour Rate -->
                 <!-- Weekly availibility -->
 
-                <v-list-item two-line class="availibilty-col">
+                <v-list-item two-line class="availibilty-col ml-lg-0 ml-md-n10 ml-sm-n12 ml-0">
                   <v-list-item-avatar size="16">
                     <img width="16" src="/images/resume_themes/theme206/icons/watch.png" />
                   </v-list-item-avatar>
