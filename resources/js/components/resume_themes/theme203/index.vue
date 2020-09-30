9599f2008dcacc9c21a88967ca896833f6ae6585
Nishad Islam <nishad.bdg@gmail.com>
2020-09-30 22:05:39 +0600

    theme 203 depressed bug fixed

--- resources/js/components/resume_themes/theme203/index.vue
+++ resources/js/components/resume_themes/theme203/index.vue
@@ -119,7 +119,7 @@
                                     bgColor="rgba(252, 210, 89, 1)"
                                     borderRadius="100"
                                     arrowColor="#000"
-                                    depressed = "true"
+                                    :depressed = "true"
                                   ></IconCarousel>
                                 </div>
                               </div>
@@ -224,7 +224,7 @@
                         bgColor="rgba(252, 210, 89, 1)"
                         borderRadius="100"
                         arrowColor="#000"
-                        depressed = "true"
+                        :depressed = "true"
                       ></IconCarousel>
                     </v-card-text>
                   </v-card>
