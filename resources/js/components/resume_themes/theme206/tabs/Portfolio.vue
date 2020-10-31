cd33bc7626e01f3cc22296a75b8015782de9e7e5
Nishad Islam <nishad.bdg@gmail.com>
2020-10-31 21:01:46 +0600

    theme 206 portfolio separated as component

---
+++ resources/js/components/resume_themes/theme206/tabs/Portfolio.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-card color="transparent" flat>
+    <v-card-text>
+      <v-row>
+        <v-col
+          cols="12"
+          sm="6"
+          md="3"
+          v-for="project in currentUser.projects"
+          :key="project.id"
+          v-show="project.is_public"
+        >
+          <v-card class="card-portfolio" hover>
+            <v-img
+              :aspect-ratio="1.2"
+              :src="getProjectMainImage(project)"
+            ></v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+<script>
+export default {
+  name: "Portfolio",
+  props: {
+    currentUser: {
+      type: Object,
+    },
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    getProjectMainImage(project) {
+      let mainImage = "";
+
+      let images = project.images;
+      images.forEach((image) => {
+        if (image.is_main) {
+          mainImage = image;
+        }
+      });
+
+      return mainImage.src;
+    },
+  },
+};
+</script>
+<style scoped lang="scss">
+.card-portfolio {
+  border-radius: 30px !important;
+}
+</style>
