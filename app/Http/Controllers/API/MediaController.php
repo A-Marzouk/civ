ea9ee67b0ffb6c385917383cf50f12e44183b9f4
Ahmed-pc <perfect_ahmed@yahoo.com>
2020-11-05 10:24:42 +0200

    - Increase the limit of media files and add error feedback.

--- app/Http/Controllers/API/MediaController.php
+++ app/Http/Controllers/API/MediaController.php
@@ -140,7 +140,7 @@ class MediaController extends Controller
             'title' => ['required', 'string', 'max:255', 'min:3'],
             'type' => ['required', 'string', 'max:255'],
             'url' => ['sometimes', 'max:255'],
-            'mediaFile' => ['sometimes', 'file', 'max:25600'], // maximum of 25 mega bytes
+            'mediaFile' => ['sometimes', 'file', 'max:51200'], // maximum of 50 mega bytes
             'mediaPreviewFile' => ['sometimes', 'file', 'max:5120','image','mimes:jpeg,png,jpg,gif,svg'], // maximum of 5 mega bytes
             'transcript' => ['max:2500'],
         ]);
