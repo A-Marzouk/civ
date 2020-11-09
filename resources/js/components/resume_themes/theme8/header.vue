ed51b6d3dc8f9613cda629a69b12ea2dda9e019c
Abdessamad El Hamdany <abdessamadelhamdany@gmail.com>
2020-11-09 22:42:17 +0100

    Override video poster, change it from image, to video poster attribute

--- resources/js/components/resume_themes/theme8/header.vue
+++ resources/js/components/resume_themes/theme8/header.vue
@@ -30,7 +30,7 @@
                     </div>
 
                     <!-- addition buttons -->
-                    <div class="hide__sm-screen">
+                    <div class="hide__sm-screen mb-3">
                         <div class="interviews">
                             <div class="text hideMeOnPhone pr-2">
                                 Interview:
