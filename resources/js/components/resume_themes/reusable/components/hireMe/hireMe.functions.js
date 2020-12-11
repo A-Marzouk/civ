f05079f5869aaee60f82aa6fbb10c7f880ad14fd
neutral-Stage <anirbanroy691@gmail.com>
2020-12-11 13:14:46 +0600

    selectable date in hireme

--- resources/js/components/resume_themes/reusable/components/hireMe/hireMe.functions.js
+++ resources/js/components/resume_themes/reusable/components/hireMe/hireMe.functions.js
@@ -24,19 +24,16 @@ export default {
             { number: '5' },
             { number: '6' },
           ],
-          model:null,
+         
           currentActiveMethod: null,
           isModalOpened: false,
           selectHours:1,
           totalHours:null,
           payToday:false,
-          currentStep: 1,
-          currentNoPaymentStep: 1,
-          iterations: 2,
-          typeOfRecurringInterval: 'week',
+          weekdayCounter:0,
+          dateSelected:null,
+          currentWeekDays:[],
           currentPaymentMethod: 'stripe',
-          currentAutoPaymentType: '7-days',
-          currentHoursType: 'week',
           // payment calculations:
           percentage: 100,
           hours:[1,2,4,6,8],
@@ -47,8 +44,6 @@ export default {
           activePaymentTypeIndex:1,
           unitHours:20,
           currentSelectedHours: 25,
-          finishedSteps: [],
-          datePicker: this.currentDate,
           isDatePickerOpened: false,
           isDateChanged: false,
           client: {
@@ -78,9 +73,6 @@ export default {
             this.$emit('modalClosed');
           }
         },
-        currentPaymentType: function(value) {
-          this.typeOfRecurringInterval = value.replace('ly', '');
-        },
         selectHours:function(value) {
             this.totalHours =  this.hours[(value-20)/20]*this.unitHours;
             
@@ -108,12 +100,32 @@ export default {
             }
           }
           this.totalHours =  this.hours[(this.selectHours-20)/20]*this.unitHours;
-          
         },
-      
-        
+        weekdayCounter:function (value) {
+          let startOfWeek = moment().add(value,'d').startOf('isoWeek');
+          var endOfWeek = moment().add(value,'d').endOf('isoWeek');
+          console.log("added days", startOfWeek)
+          var days = [];
+          var day = startOfWeek;
+          while (day <= endOfWeek) {
+            let dayObj = {
+              obj: day.format('YYYY-MM-DD'),
+              weekday: day.format('ddd'),
+              date: day.format('DD'),
+            }
+              days.push(dayObj);
+              day = day.clone().add(1, 'd');
+          }
+          return  this.currentWeekDays=days;
+        }
       },
       methods: {
+        increaseCounter() { // Increase
+           this.weekdayCounter+=7;
+        },
+        decreaseCounter() { // Decrease
+           this.weekdayCounter-=7;
+        }, 
         payableToday(){
           this.payToday = true;
           this.isDatePickerOpened = false;
@@ -130,169 +142,14 @@ export default {
             this.activePaymentTypeIndex = 2;
           }
         },
-     
-
-        goToNextStep() {
-          if (this.currentStep > 4) {
-            return;
-          }
-          this.finishedSteps.push(this.currentStep);
-          this.currentStep++;
-        },
-        intervalNumberPlaceHolder() {
-          if (this.currentPaymentType === 'hourly') {
-            return '';
-          }
-          return 'Number of ' + this.typeOfRecurringInterval + 's';
-        },
-        getHoursLabel() {
-          if (this.currentPaymentType === 'weekly') {
-            return 'Weekly';
-          }
-          if (this.currentPaymentType === 'monthly') {
-            return 'Monthly';
-          }
-          return 'Hours';
-        },
-        goToPreviousStep() {
-          if (this.currentStep < 2) {
-            return;
-          }
-          this.currentStep--;
-        },
-        isStepActive(step) {
-          return step === this.currentStep;
-        },
-        isStepDone(step) {
-          return this.finishedSteps.includes(step);
-        },
-    
-        changePaymentMethod(payment_method) {
-          this.currentPaymentMethod = payment_method;
-          this.percentage = 100;
-        },
-        // step 2
-        setPaymentType(payment_type) {
-          this.currentPaymentType = payment_type;
-        },
-        isPaymentTypeActive(payment_type) {
-          return this.currentPaymentType === payment_type;
-        },
-    
-        // step 3
-        subtractHours() {
-          if (this.currentSelectedHours < 10) {
-            return;
-          }
-          this.currentSelectedHours -= 5;
-        },
-        addHours() {
-          if (this.currentSelectedHours > 1000) {
-            return;
-          }
-          this.currentSelectedHours += 5;
-        },
-        isHoursTypeActive(hours_type) {
-          return this.currentHoursType === hours_type;
-        },
-        setHoursType(hours_type) {
-          this.currentHoursType = hours_type;
-        },
-        reset() {
-          this.currentStep = 1;
-          this.finishedSteps = [];
-          this.currentSelectedHours = 40;
-          this.closeModal();
-        },
-        continueToSend() {
-          if (this.validateInputs()) {
-            this.currentNoPaymentStep = 3;
-          }
-        },
-        pay() {
-          if (this.validateInputs()) {
-            this.paymentData = {
-              client: this.client,
-              freelancer: this.user,
-              payment_info: {
-                numberOfHours: this.currentSelectedHours,
-                totalAmount: this.totalPaymentAmount,
-                percentage: this.percentage,
-                toPayNowAmount: this.currentPaymentAmount,
-                toPayLaterAmount: this.totalPaymentAmount - this.currentPaymentAmount,
-                toPayLaterDate: this.payLaterDate,
-                iterations: this.iterations,
-                isRecurring: this.currentPaymentType === 'weekly' || this.currentPaymentType === 'monthly',
-                interval: this.currentPaymentType === 'weekly' ? 'week' : 'month'
-              }
-            };
-    
-            let paymentUrl = '/custom-stripe-payment';
-            if (this.currentPaymentMethod === 'paypal') {
-              paymentUrl = '/custom-paypal-payment';
-            }
-    
-            axios
-              .post(paymentUrl, this.paymentData)
-              .then(response => {
-                window.location = response.data.url;
-              })
-              .catch(error => {
-                console.log(error);
-              });
-          }
-        },
-        validateInputs() {
-          let isValid = true;
-          this.errors = {};
-    
-          if (this.client.name.length < 2 || this.client.name.length > 200) {
-            isValid = false;
-            this.errors.name = 'Name should be at least 2 characters';
-          }
-          if (!this.validateEmail(this.client.email)) {
-            isValid = false;
-            this.errors.email = 'Email should be a valid format';
-          }
-    
-          if (!this.validatePhone(this.client.phone)) {
-            isValid = false;
-            this.errors.phone = 'Phone should be a valid format';
-          }
-    
-          return isValid;
-        },
-        validateEmail(email) {
-          const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-          return re.test(String(email).toLowerCase());
-        },
-    
-        validatePhone(phone) {
-          const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
-          return re.test(String(phone));
+        selectedDate(date){
+          this.dateSelected = date;
         },
         closeModal() {
           this.hireMeModal = false;
         },
-    
-        // step4
-        getPayLink() {
-          let paymentMethod = this.user.payment_methods.find(
-            i => i.name.toLowerCase() === this.currentPaymentMethod.toLowerCase()
-          );
-          if (paymentMethod) {
-            return paymentMethod.link;
-          }
-          return '#';
-        },
         moment: function() {
           return moment();
-        },
-        isAutoPaymentTypeActive(payment_type) {
-          return this.currentAutoPaymentType === payment_type;
-        },
-        setAutoPaymentType(payment_type) {
-          this.currentAutoPaymentType = payment_type;
         },
         pickDateSelected() {
           this.currentAutoPaymentType = 'custom-date';
@@ -304,22 +161,13 @@ export default {
           this.isDatePickerOpened = false;
           this.isDateChanged = true;
         },
-        validateAndGoToNextStep() {
-          if (this.validateInputs()) {
-            this.goToNextStep();
-          }
-        }
       },
       computed: {
         paymentTotal(){
           return this.totalHours * this.user.payment_info[0].salary;
         },
         monthsNumber(){
-         
           return moment().format("M");
-        },
-        currentPaymentAmount() {
-          return ((this.user.payment_info[0].salary * this.percentage) / 100) * this.currentSelectedHours;
         },
         totalPaymentAmount() {
           return this.user.payment_info[0].salary * this.currentSelectedHours;
@@ -334,39 +182,18 @@ export default {
           }
           return months;
         },
-        getArrayOfDays(){
-          
-          return moment("2012-01", "YYYY-MM").daysInMonth()
-        },
-        payLaterDate() {
-          if (this.currentAutoPaymentType === '7-days') {
-            return moment()
-              .add(7, 'days')
-              .format('YYYY-MM-DD');
-          } else if (this.currentAutoPaymentType === '14-days') {
-            return moment()
-              .add(14, 'days')
-              .format('YYYY-MM-DD');
-          }
-    
-          return this.datePicker;
-        },
+        
         userHourlyRate() {
           return this.user.payment_info[0].salary;
-        },
-        currentPosition() {
-          return 100 - this.percentage - 10;
-        },
-        currentHoursPosition() {
-          return 100 - this.currentSelectedHours * 2 - 10;
         },
         paymentMethods() {
           return this.user.payment_methods;
         },
         currentDate() {
-          return moment()
-            .add(1, 'days')
-            .format('YYYY-MM-DD');
+          if(this.dateSelected==null){
+            return moment().format('YYYY-MM-DD');
+          }
+          return this.dateSelected;
         },
         futureDate() {
           if (this.currentPaymentType === 'monthly') {
@@ -378,5 +205,21 @@ export default {
             .add(7, 'days')
             .format('YYYY-MM-DD');
         }
+      },
+      mounted(){
+        var startOfWeek = moment().startOf('isoWeek');
+        var endOfWeek = moment().endOf('isoWeek');
+        var days = [];
+        var day = startOfWeek;
+        while (day <= endOfWeek) {
+          let dayObj = {
+            obj: day.format('YYYY-MM-DD'),
+            weekday: day.format('ddd'),
+            date: day.format('DD'),
+          }
+            days.push(dayObj);
+            day = day.clone().add(1, 'd');
+        }
+        return  this.currentWeekDays= days;
       }
 }
--- resources/js/components/resume_themes/reusable/components/hireMe/hireMe.vue
+++ resources/js/components/resume_themes/reusable/components/hireMe/hireMe.vue
@@ -1,7 +1,7 @@
 <template>
   <div data-app>
     <v-row justify="center">
-      <v-dialog v-model="isModalOpened" persistent max-width="600" >
+      <v-dialog v-model="isModalOpened" persistent max-width="600">
         <v-card>
           <v-toolbar dark color="#f2f3fd" flat>
             <v-toolbar-title class="main-color pl-4">Booking/ payment</v-toolbar-title>
@@ -16,7 +16,8 @@
                 <!-- Profile Info -->
                 <v-row dense>
                   <v-col cols="auto"
-                    ><v-avatar size="100"> <img :src="user.personal_info.profile_pic" :alt="user.personal_info.first_name" /> </v-avatar
+                    ><v-avatar size="100">
+                      <img :src="user.personal_info.profile_pic" :alt="user.personal_info.first_name" /> </v-avatar
                   ></v-col>
                   <v-col cols="9">
                     <div class="user-name">
@@ -35,7 +36,7 @@
                     ><v-icon small color="#888DB1" @click="prevPaymentType()">mdi-chevron-left</v-icon></v-btn
                   >
                   <div class="monthly">
-                    <div class="monthly_text">{{paymentTypes[activePaymentTypeIndex]}}ly</div>
+                    <div class="monthly_text">{{ paymentTypes[activePaymentTypeIndex] }}ly</div>
                   </div>
                   <v-btn fab width="18" height="18" class="elevation" color="white"
                     ><v-icon small color="#888DB1" @click="nextPaymentType()">mdi-chevron-right</v-icon></v-btn
@@ -54,7 +55,7 @@
                     </v-col>
                     <v-col cols="12">
                       <div class="perDayNumber">
-                        <div v-for="(n, i) in hours" :key="i" class="number ">
+                        <div v-for="(n, i) in hours" :key="i" class="number">
                           {{ n }}
                         </div>
                       </div>
@@ -79,7 +80,7 @@
                       </div>
                     </v-col>
                     <v-col cols="12">
-                      <div class="d-inline-block select-tag">Per {{paymentTypes[activePaymentTypeIndex]}}</div>
+                      <div class="d-inline-block select-tag">Per {{ paymentTypes[activePaymentTypeIndex] }}</div>
                     </v-col>
                   </v-row>
                 </v-container>
@@ -92,21 +93,23 @@
                   <v-row>
                     <v-col cols="4" class="text-center">
                       <div class="rate-label mb-3">Hourly rate</div>
-                      <input
-                        type="text"
-                        class="rate-input text"
-                        name="hourly_rate"
-                        :value="'$' + userHourlyRate"
-                      />
+                      <input type="text" class="rate-input text" name="hourly_rate" :value="'$' + userHourlyRate" />
                       <div class="operators">x</div>
                     </v-col>
                     <v-col cols="4" class="text-center"
-                      ><div class="rate-label mb-3">No. of hours/ {{ paymentTypes[activePaymentTypeIndex] == 'hour' ? 'day' : paymentTypes[activePaymentTypeIndex] }}</div>
+                      ><div class="rate-label mb-3">
+                        No. of hours/
+                        {{
+                          paymentTypes[activePaymentTypeIndex] == 'hour' ? 'day' : paymentTypes[activePaymentTypeIndex]
+                        }}
+                      </div>
                       <input type="text" class="rate-input text" name="no_of_week" :value="totalHours" />
                       <div class="operators">=</div>
                     </v-col>
                     <v-col cols="4" class="text-center"
-                      ><div class="rate-label mb-3"> <span class="text-capitalize">{{paymentTypes[activePaymentTypeIndex]}}ly</span>  payment</div>
+                      ><div class="rate-label mb-3">
+                        <span class="text-capitalize">{{ paymentTypes[activePaymentTypeIndex] }}ly</span> payment
+                      </div>
 
                       <input type="text" class="rate-input total" name="weekly-rate" :value="'$' + paymentTotal" />
                     </v-col>
@@ -119,7 +122,9 @@
               <v-col cols="12" class="total-payment">
                 <v-row no-gutters>
                   <v-col cols="9">
-                    <div class="total-payment_text pl-6">Your {{paymentTypes[activePaymentTypeIndex]}}ly payment will be</div>
+                    <div class="total-payment_text pl-6">
+                      Your {{ paymentTypes[activePaymentTypeIndex] }}ly payment will be
+                    </div>
                   </v-col>
                   <v-col cols="2">
                     <div class="total-payment_value pl-3">${{ paymentTotal }}</div>
@@ -130,80 +135,76 @@
               <!-- Payment Options -->
               <v-col cols="12">
                 <v-btn :color="payToday ? '#001D68' : '#F2F3FD'" :dark="payToday" depressed @click="payableToday">
-                  <div class="payment-text" :class="[payToday? 'white--text' : 'main-color']" >Payable today</div>
+                  <div class="payment-text" :class="[payToday ? 'white--text' : 'main-color']">Payable today</div>
                 </v-btn>
                 <div class="d-inline-block or px-4">or</div>
-                <v-btn :color="isDatePickerOpened ?  '#001D68' : '#F2F3FD'" depressed @click="pickDateSelected" :dark="isDatePickerOpened">
-                  <div class="payment-text" :class="[isDatePickerOpened? 'white--text' : 'main-color']">Date picker</div>
+                <v-btn
+                  :color="isDatePickerOpened ? '#001D68' : '#F2F3FD'"
+                  depressed
+                  @click="pickDateSelected"
+                  :dark="isDatePickerOpened"
+                >
+                  <div class="payment-text" :class="[isDatePickerOpened ? 'white--text' : 'main-color']">
+                    Date picker
+                  </div>
                 </v-btn>
               </v-col>
               <!-- Payment Options -->
               <!-- Date Picker -->
               <v-col cols="12" v-show="isDatePickerOpened">
-                  <v-row dense>
-                    <v-col cols="12">
-                    <v-menu
-                      bottom
-                      origin="center center"
-                      transition="scale-transition"
-                    >
+                <v-row dense>
+                  <v-col cols="12">
+                    <v-menu bottom origin="center center" transition="scale-transition">
                       <template v-slot:activator="{ on, attrs }">
-                        <v-btn
-                          color="#001D68"
-                          dark
-                          v-bind="attrs"
-                          v-on="on"
-                          text
-                          class="px-0"
-                          
-                        >
-                          {{getArrayOfMonths[0]}} <v-icon right>mdi-calendar-blank</v-icon>
+                        <v-btn color="#001D68" dark v-bind="attrs" v-on="on" text class="px-0">
+                          {{ getArrayOfMonths[0] }} <v-icon right>mdi-calendar-blank</v-icon>
                         </v-btn>
                       </template>
 
                       <v-list>
-                        <v-list-item
-                          v-for="(item, i) in getArrayOfMonths"
-                          :key="i"
-                        >
+                        <v-list-item v-for="(item, i) in getArrayOfMonths" :key="i">
                           <v-list-item-title>{{ item }}</v-list-item-title>
                         </v-list-item>
                       </v-list>
                     </v-menu>
-                    </v-col>
-                    <v-col cols="auto">
-                      <v-btn fab width="18" height="18" class="elevation" color="white"
-                        ><v-icon small color="#888DB1" @click="prevPaymentType()">mdi-chevron-left</v-icon></v-btn
+                  </v-col>
+                  <v-col cols="auto">
+                    <v-btn fab width="18" height="18" class="elevation" color="white"
+                      ><v-icon small color="#888DB1" @click="decreaseCounter">mdi-chevron-left</v-icon></v-btn
+                    >
+                  </v-col>
+                  <v-col cols="auto">
+                    <div class="week-days">
+                      <div
+                        v-for="(day, i) in currentWeekDays"
+                        :key="i"
+                        class="week-days_single"
+                        :class="currentDate == day.obj ? 'active' : 'inactive'"
+                        @click="selectedDate(day.obj)"
                       >
-                    </v-col>
-                    <v-col cols="auto">
-                      <div class="week-days">
-                        <div v-for="(n, i) in 7" :key="i" class="week-days_single">
-                          <div class="weekday">
-                            TUE
-                          </div>
-                          <div class="day">0{{ n }}</div> 
-                          <!-- <div >●</div> -->
+                        <div class="weekday">
+                          {{ day.weekday }}
                         </div>
+                        <div class="day">{{ day.date }}</div>
+                        <div v-show="currentDate == day.obj">●</div>
                       </div>
-                    </v-col>
-                    <v-col cols="auto">
-                      <v-btn fab width="18" height="18" class="elevation" color="white"
-                        ><v-icon small color="#888DB1" @click="nextPaymentType()">mdi-chevron-right</v-icon></v-btn
-                      >
-                    </v-col>
-                  </v-row>
-               
+                    </div>
+                    <!-- <div>{{ weekdayCounter }}</div> -->
+                  </v-col>
+                  <v-col cols="auto">
+                    <v-btn fab width="18" height="18" class="elevation" color="white"
+                      ><v-icon small color="#888DB1" @click="increaseCounter">mdi-chevron-right</v-icon></v-btn
+                    >
+                  </v-col>
+                </v-row>
               </v-col>
-              
               <!-- Date Picker -->
             </v-row>
             <!-- Payment Form -->
             <v-col cols="12">
-              
-              <v-row no-gutters v-show="activePaymentTypeIndex !=0">
+              <v-row no-gutters v-show="activePaymentTypeIndex != 0">
                 <v-col cols="auto">
-                  <div class="month-numbers">Number of {{paymentTypes[activePaymentTypeIndex]}}’s -</div>
+                  <div class="month-numbers">Number of {{ paymentTypes[activePaymentTypeIndex] }}’s -</div>
                 </v-col>
                 <v-col cols="3">
                   <div style="max-width: 72%">
@@ -221,7 +222,6 @@
                       flat
                     ></v-select>
                   </div>
-
                 </v-col>
               </v-row>
 
@@ -294,11 +294,9 @@ export default {
 
 <style lang="scss" scoped>
 @import './hireMe.styles.scss';
-
 </style>
 
 <style lang="scss">
-
 .hourSlider {
   .v-slider--horizontal {
     min-height: 25px !important;
