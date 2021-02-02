2d1033fcc38767ebf40f37bcc1ff623e85e02452
Ahmed-pc <perfect_ahmed@yahoo.com>
2021-02-02 12:28:14 +0200

    - Auth page | update icons | change same place

--- resources/js/components/auth/AuthForm.vue
+++ resources/js/components/auth/AuthForm.vue
@@ -349,8 +349,7 @@
     .auth-main-wrapper{
         display: flex;
         justify-content: center;
-        align-items: center;
-        min-height: 100vh;
+        margin-top:100px;
 
 
         .auth-form{
@@ -451,6 +450,9 @@
                     box-sizing: border-box;
                     border-radius: 10px;
                     margin-left: 40px;
+                    img{
+                        width: 20px;
+                    }
                 }
 
                 @media screen and (max-width: 660px) {
