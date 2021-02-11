10d4d0254a981c5d142983d44a5d14a5cbc91e83
Ahmed-pc <perfect_ahmed@yahoo.com>
2021-02-11 18:57:09 +0200

    - Default theme first optimization phase.

--- resources/views/defaultThemes/theme1001.blade.php
+++ resources/views/defaultThemes/theme1001.blade.php
@@ -1,7 +1,21 @@
-@extends('layouts.my-app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link href="/css/app.css" rel="stylesheet">
+    <title>civ.ie</title>
+    <link rel="icon" href="/images/welcome_landing_page/logo/civie_logo_res-01.png" type="image/x-icon">
+</head>
+<body >
     <div id="resumeTheme1001">
         <resume-theme-1001 @if(isset($user)) :user="{{$user}}" @endif :is_preview="{{$is_preview}}"></resume-theme-1001>
     </div>
-@endsection
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="/js/themes/theme1001.js"></script>
+</body>
+</html>
+
