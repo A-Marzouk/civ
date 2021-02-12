2320f79a63fe75a413e4f478fb1813c3d4ecedd8
Ahmed-pc <perfect_ahmed@yahoo.com>
2021-02-12 19:16:42 +0200

    - resume builder - blue text on focus.

--- resources/views/defaultThemes/theme1001.blade.php
+++ resources/views/defaultThemes/theme1001.blade.php
@@ -16,6 +16,8 @@
 
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script type="text/javascript" src="/js/themes/theme1001.js"></script>
+    <script type="text/javascript" src="/js/app.js"></script>
+
 </body>
 </html>
 
