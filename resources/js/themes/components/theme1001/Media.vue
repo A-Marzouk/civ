6e9b373d8d41700fa8c6b91173e85bba776eebcf
Ahmed-pc <perfect_ahmed@yahoo.com>
2021-02-16 19:55:18 +0200

    - adds media tab | second phase

--- resources/js/themes/components/theme1001/Media.vue
+++ resources/js/themes/components/theme1001/Media.vue
@@ -84,6 +84,38 @@
                 <img class="play-icon" src="/images/themes/theme1001/media/audio-play-btn.png" alt="play btn">
             </div>
         </div>
+
+        <div class="media-player-footer">
+            <div class="media-info">
+                <div class="preview">
+                    <img src="/images/themes/theme1001/media/audio-preview.png" alt="preview">
+                </div>
+                <div class="details">
+                    <div class="title">
+                        AUD17/01/2021
+                    </div>
+                    <div class="name">
+                        Hean Prinsloo
+                    </div>
+                    <div class="time">
+                        00:00:00
+                    </div>
+                </div>
+            </div>
+
+            <div class="buttons">
+                <img src="/images/themes/theme1001/media/shuffle-button.png" alt="shuffle">
+                <img class="prev" src="/images/themes/theme1001/media/buttun-prev.png" alt="prev">
+                <img src="/images/themes/theme1001/media/button-play.png" alt="play">
+                <img class="next"  src="/images/themes/theme1001/media/button-next.png" alt="next">
+                <img src="/images/themes/theme1001/media/repeat-button.png" alt="repeat">
+            </div>
+            <div class="playing">
+                <div class="time">00:00</div>
+                <img src="/images/themes/theme1001/media/music-action.png" alt="music">
+                <div class="time">5:38</div>
+            </div>
+        </div>
     </div>
 </template>
 
